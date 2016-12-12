90a336c55632cba44f9557a30f5ecf9a90a40683
André Wallat <andre.wallat@wemove.com>
2016-12-12 09:58:40 +0100

    - improved help flyover component with animations

--- app/help/help.component.ts
+++ app/help/help.component.ts
@@ -1,19 +1,45 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, animate, transition, style, state, trigger} from "@angular/core";
+import {Router, NavigationEnd} from "@angular/router";
 
 @Component({
   selector: 'help-panel',
   template: require('./help.component.html'),
-  styles: [require('./help.component.css')]
+  styles: [require('./help.component.css')],
+  animations: [
+    trigger('openClose', [
+      state('collapsed, void', style({ transform: 'translate3d(100%, 0, 0)' })),
+      state('expanded', style({ transform: 'translate3d(0, 0, 0)', width: '300px' })),
+      state('maximized', style({ transform: 'translate3d(0, 0, 0)', width: '95%' })),
+      transition("collapsed => expanded", animate('200ms ease-in')),
+      transition("* => collapsed", animate('200ms ease-out')),
+      transition("expanded => maximized", animate('200ms ease-in')),
+      transition("maximized => expanded", animate('200ms ease-out'))
+    ])
+  ]
 })
 export class HelpComponent implements OnInit {
 
-  showHelp: boolean = false;
-  expanded: boolean = false;
+  menuState = 'collapsed';
+  text: string;
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        console.log('fetch help:', event.url);
+        this.text = event.url;
+      }
+    });
   }
 
+  toggleState() {
+    this.menuState === 'collapsed' ? this.menuState = 'expanded' : this.menuState = 'collapsed';
+  }
+
+  toggleMaximized() {
+    console.log( 'max' );
+    this.menuState === 'maximized' ? this.menuState = 'expanded' : this.menuState = 'maximized';
+  }
 }
