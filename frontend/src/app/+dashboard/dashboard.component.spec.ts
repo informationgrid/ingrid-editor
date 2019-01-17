3ddf8d4ac08a3034fc942d5f22b58c2ead374e97
André Wallat <andre.wallat@wemove.com>
2019-01-17 21:49:30 +0100

    Add test for dashboard

--- frontend/src/app/+dashboard/dashboard.component.spec.ts
+++ frontend/src/app/+dashboard/dashboard.component.spec.ts
@@ -1,7 +1,22 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {DashboardComponent} from "./dashboard.component";
 import {DebugElement} from "@angular/core";
 import {By} from "@angular/platform-browser";
+import {ChartistModule} from "ng-chartist";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {ConfigDataService} from "../services/config/config-data.service";
+import {MatDialogModule} from "@angular/material";
+import {DocumentDataService} from "../services/document/document-data.service";
+import {RouteConfigLoadStart} from "@angular/router";
+import {ConfigService} from "../services/config/config.service";
+import {DocumentService} from "../services/document/document.service";
+import {HttpClient} from "@angular/common/http";
+import {ConfigMockService} from "../services/config/config-mock.service";
+import {DocumentMockService} from "../services/document/document-mock.service";
+import {CodelistService} from "../services/codelist/codelist.service";
+import {CodelistMockService} from "../services/codelist/codelist-mock.service";
+import {CodelistDataService} from "../services/codelist/codelist-data.service";
 
 
 describe('DashboardComponent', () => {
@@ -10,7 +25,13 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [DashboardComponent],
+      imports: [ChartistModule, RouterTestingModule, HttpClientTestingModule, MatDialogModule],
+      providers: [
+        {provide: ConfigDataService, useClass: ConfigMockService},
+        {provide: CodelistDataService, useClass: CodelistMockService},
+        {provide: DocumentDataService, useClass: DocumentMockService}
+      ]
     })
       .compileComponents();
   }));
@@ -27,8 +48,8 @@ describe('DashboardComponent', () => {
 
   it('should show last recent documents', () => {
     let dashboardDe: DebugElement = fixture.debugElement;
-    const recentDocs = dashboardDe.query(By.css('recentDocs'));
+    const recentDocs = dashboardDe.query(By.css('.recentDocs'));
     let recentDocsEl: HTMLElement = recentDocs.nativeElement;
-    expect(recentDocsEl.textContent).toEqual('Letzte Dokumente');
+    expect(recentDocsEl.textContent.trim()).toEqual('Letzte Dokumente');
   });
 });
--- frontend/src/app/+dashboard/dashboard.module.ts
+++ frontend/src/app/+dashboard/dashboard.module.ts
@@ -1,20 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {routing} from './dashboard.routing';
 import {DashboardComponent} from './dashboard.component';
 import {ChartistComponent} from "./chartist.component";
 import {RouterModule, Routes} from "@angular/router";
+import {AuthGuard} from "../security/auth.guard";
+import {NoCatalogAssignedGuard} from "../security/no-catalog-assigned.guard";
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: DashboardComponent,
+    canActivate: [AuthGuard, NoCatalogAssignedGuard],
+    data: {roles: ['admin', 'author']}
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), CommonModule, routing],
+  imports: [RouterModule.forChild(routes), CommonModule],
   declarations: [DashboardComponent, ChartistComponent],
   exports: [RouterModule]
 })
-export class DashboardModule {}
+export class DashboardModule {
+}
