d111cf16524757e1f9fff0a5733f330ca37fb814
André Wallat <andre.wallat@wemove.com>
2019-07-10 20:34:52 +0200

    Add ngx-formly and add story

--- frontend/src/stories/index.stories.ts
+++ frontend/src/stories/index.stories.ts
@@ -4,11 +4,16 @@ import {linkTo} from '@storybook/addon-links';
 
 import {Welcome, Button} from '@storybook/angular/demo';
 import {DateboxComponent} from '../app/form-fields/datebox/datebox.component';
-import {MatDatepickerModule} from '@angular/material';
+import {MatCardModule, MatDatepickerModule, MatInputModule} from '@angular/material';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FormFieldsModule} from '../app/form-fields/form-fields.module';
 import {CommonModule} from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormlyModule} from '@ngx-formly/core';
+import {FormlyFieldConfig} from '@ngx-formly/core/lib/components/formly.field.config';
+import {FormlyMaterialModule} from '@ngx-formly/material';
+import {OneColumnWrapperComponent} from '../app/formly/wrapper/one-column-wrapper.component';
+import {IgeFormModule} from '../app/+form/ige-form.module';
 
 storiesOf('Welcome', module).add('to Storybook', () => ({
   component: Welcome,
@@ -70,6 +75,50 @@ storiesOf('Datebox', module).add('simple', () => ({
     form: new FormGroup({
       birthday: new FormControl(1562620600475)
     })
+
+  }
+}));
+storiesOf('Formly', module).add('input', () => ({
+  // component: ,
+  moduleMetadata: {
+    imports: [
+      ReactiveFormsModule,
+      IgeFormModule,
+      BrowserAnimationsModule,
+      MatCardModule,
+      FormlyMaterialModule,
+      FormlyModule.forRoot({
+        wrappers: [
+          { name: 'panel', component: OneColumnWrapperComponent },
+        ],
+      })
+    ],
+    declarations: [OneColumnWrapperComponent]
+  },
+
+  template: `<form [formGroup]="form">
+                <formly-form [form]="form" [fields]="fields" [model]="model"></formly-form>
+            </form>
+            {{model | json}} {{form.valid}}`,
+  props: {
+    model: {},
+    form: new FormGroup({}),
+    fields: <FormlyFieldConfig[]>[{
+      key: 'ip',
+      type: 'input',
+      wrappers: ['panel', 'form-field'],
+      templateOptions: {
+        label: 'IP address',
+        placeholder: 'Enter IP',
+        required: true,
+      },
+      validators: {
+        ip: {
+          expression: (c) => /(\d{1,3}\.){3}\d{1,3}/.test(c.value),
+          message: (error, field: FormlyFieldConfig) => `"${field.formControl.value}" is not a valid IP Address`,
+        },
+      },
+    }]
 
   }
 }));
