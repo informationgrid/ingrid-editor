bbe9ba967659b042326c023f40b1563de5483a73
André Wallat <andre.wallat@wemove.com>
2020-01-20 19:04:58 +0100

    Setup for kotlin and convert startup class

--- server/src/main/java/de/ingrid/igeserver/IgeServer.kt
+++ server/src/main/java/de/ingrid/igeserver/IgeServer.kt
@@ -1,36 +1,17 @@
-package de.ingrid.igeserver;
+package de.ingrid.igeserver
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.ExitCodeGenerator;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
 
 @SpringBootApplication
-public class IgeServer implements CommandLineRunner {
+class IgeServer
 
-    private static Logger log = LogManager.getLogger(IgeServer.class);
+private val log = LogManager.getLogger(IgeServer::class.java)
 
-    @Override
-    public void run(String... arg0) {
-        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
-            throw new ExitException();
-        }
-    }
+fun main(args: Array<String>) {
+    log.info("Starting application")
+    SpringApplication(IgeServer::class.java).run(*args)
 
-    public static void main(String[] args) {
-        log.info("Starting application");
-        new SpringApplication(IgeServer.class).run(args);
-    }
-
-    class ExitException extends RuntimeException implements ExitCodeGenerator {
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public int getExitCode() {
-            return 10;
-        }
-
-    }
 }
