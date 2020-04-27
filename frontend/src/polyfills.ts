6527c7b3ec0f3960db104dc9fce135d2de25adf3
André Wallat <andre.wallat@wemove.com>
2020-04-27 22:00:22 +0200

    IE11 fixes

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -20,7 +20,7 @@
 
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 import 'web-animations-js';  // Run `npm install --save web-animations-js`.
@@ -53,10 +53,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  */
 // import 'intl';  // Run `npm install --save intl`.
 
-// import 'rxjs/add/observable/of';
-/*import 'rxjs/add/observable/interval';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';*/
