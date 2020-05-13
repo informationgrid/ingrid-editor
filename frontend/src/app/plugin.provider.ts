c18f7a49c84927eb39a8c6a342d2191e0000fb86
André Wallat <andre.wallat@wemove.com>
2020-05-13 19:02:35 +0200

    Configurable creation of address title (#1885)

--- frontend/src/app/plugin.provider.ts
+++ frontend/src/app/plugin.provider.ts
@@ -1,8 +1,10 @@
 import {PluginToken} from './tokens/plugin.token';
 import {SortTreeByTypeBehaviour} from './+behaviours/system/SortTreeByType/sort-tree-by-type.behaviour';
 import {DeleteEmptyFoldersBehaviour} from './+behaviours/system/DeleteEmptyFolders/delete-empty-folders.behaviour';
+import {AddressTitleBehaviour} from './+behaviours/system/AddressTitle/address-title.behaviour';
 
 export const pluginProvider = [
   {provide: PluginToken, useClass: SortTreeByTypeBehaviour, multi: true},
   {provide: PluginToken, useClass: DeleteEmptyFoldersBehaviour, multi: true},
+  {provide: PluginToken, useClass: AddressTitleBehaviour, multi: true}
 ];
