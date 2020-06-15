0dd3d1c8fba487ff438106414063795860cb4594
André Wallat <andre.wallat@wemove.com>
2020-06-15 12:54:02 +0200

    Add spatial test definitions

---
+++ frontend/e2e/cypress/integration/documents/spatial.spec.ts
@@ -0,0 +1,30 @@
+describe('Spatial References', () => {
+  beforeEach(() => {
+    cy.kcLogin('user');
+  })
+
+  xit('should create a new spatial reference (bbox)', () => {
+
+  });
+
+  xit('should create a new spatial reference (WKT)', () => {
+
+  });
+
+  xit('should update a spatial reference (bbox)', () => {
+
+  });
+
+  xit('should update a spatial reference (WKT)', () => {
+
+  });
+
+  xit('should remove spatial references', () => {
+
+  });
+
+  xit('should focus on a selected spatial reference and also reset view after pressing reset button', () => {
+
+  });
+
+});
