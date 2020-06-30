a15b8c3b8d5d4a294772c1eaea02cb1c0691373c
André Wallat <andre.wallat@wemove.com>
2020-06-30 12:37:43 +0200

    Close icon button fixes

--- frontend/src/app/+form/form.utils.ts
+++ frontend/src/app/+form/form.utils.ts
@@ -1,11 +1,9 @@
-import {DocumentService} from '../services/document/document.service';
-import {FormGroup} from '@angular/forms';
 import {FormToolbarService} from './form-shared/toolbar/form-toolbar.service';
 
 export class FormUtils {
   static addHotkeys(event: KeyboardEvent, service: FormToolbarService) {
     if (event.ctrlKey && event.key === 's') { // CTRL + S (Save)
-      console.log( 'SAVE' );
+      console.log('SAVE');
       event.stopImmediatePropagation();
       event.stopPropagation();
       event.preventDefault();
