c095e007dbedf66735f88a5ab488637c0e25627f
André Wallat <andre.wallat@wemove.com>
2020-07-01 13:51:53 +0200

    Add missing source file

---
+++ frontend/src/app/+catalog/behaviours-changed.guard.ts
@@ -0,0 +1,32 @@
+import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {CatalogManagerComponent} from './catalog-manager/catalog-manager.component';
+import {MatDialog} from '@angular/material/dialog';
+import {ConfirmDialogComponent, ConfirmDialogData} from '../dialogs/confirm/confirm-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BehavioursChangedGuard implements CanDeactivate<CatalogManagerComponent> {
+
+  constructor(private dialog: MatDialog) {
+  }
+
+  canDeactivate(component: CatalogManagerComponent, route: ActivatedRouteSnapshot,
+                state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+    const behavioursHaveChanged = component.behaviourComponent.hasDirtyForm();
+
+    if (behavioursHaveChanged) {
+      return this.dialog.open(ConfirmDialogComponent, {
+        data: {
+          title: 'Änderungen verwerfen?',
+          message: 'Wollen Sie die Änderungen an den Verhalten verwerfen?'
+        } as ConfirmDialogData
+      }).afterClosed();
+    }
+    return true;
+
+  }
+}
