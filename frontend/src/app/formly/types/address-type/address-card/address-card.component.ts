824f98d6d06aa73d01ed651a5cb9c275ba628ffe
André Wallat <andre.wallat@wemove.com>
2020-07-07 17:30:54 +0200

    Improve address cards (#1966)

--- frontend/src/app/formly/types/address-type/address-card/address-card.component.ts
+++ frontend/src/app/formly/types/address-type/address-card/address-card.component.ts
@@ -1,9 +1,10 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {IgeDocument} from '../../../../models/ige-document';
+import {ProfileService} from '../../../../services/profile.service';
 
 export interface AddressRef {
   type: string;
-  ref: Partial<IgeDocument>;
+  ref: Partial<any>;
 }
 
 @Component({
@@ -17,12 +18,47 @@ export class AddressCardComponent implements OnInit {
   @Input() address: AddressRef;
   @Output() remove = new EventEmitter();
   @Output() edit = new EventEmitter();
-  cardClass = 'no-box-shadow';
 
-  constructor() {
+  content: {
+    iconClass: string;
+    role: string;
+    title: string;
+    secondTitle: string;
+    emailOrPhone: string;
+  }
+
+  constructor(private profileService: ProfileService) {
   }
 
   ngOnInit(): void {
+    this.content = {
+      iconClass: this.profileService.getDocumentIcon(<IgeDocument>this.address.ref),
+      role: this.address.type,
+      title: this.getTitle(this.address.ref),
+      secondTitle: this.getSecondTitle(this.address.ref),
+      emailOrPhone: this.getEmailOrTelephone(this.address.ref)
+    }
   }
 
+  private getEmailOrTelephone(address: any) {
+    // TODO: type is a string but should be number?
+    const email = address.contact?.filter(item => item.type === '3');
+    if (email && email.length > 0) {
+      return email[0].connection;
+    } else {
+      return '???';
+    }
+  }
+
+  private getTitle(ref: any) {
+    return ref.organization?.length > 0
+      ? ref.organization
+      : ref.firstName + ' ' + ref.lastName;
+  }
+
+  private getSecondTitle(ref: any) {
+    return ref.organization?.length > 0
+      ? ref.firstName + ' ' + ref.lastName
+      : null;
+  }
 }
