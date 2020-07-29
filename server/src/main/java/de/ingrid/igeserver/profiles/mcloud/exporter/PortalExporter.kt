0ba13d7f5647025afbb6c472bfb0be5b43522b7d
André Wallat <andre.wallat@wemove.com>
2020-07-29 17:02:23 +0200

    Create export format for mcloud document to Elasticsearch

---
+++ server/src/main/java/de/ingrid/igeserver/profiles/mcloud/exporter/PortalExporter.kt
@@ -0,0 +1,54 @@
+package de.ingrid.igeserver.profiles.mcloud.exporter
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.mitchellbosecke.pebble.PebbleEngine
+import de.ingrid.igeserver.exports.ExportTypeInfo
+import de.ingrid.igeserver.exports.IgeExporter
+import de.ingrid.igeserver.profiles.mcloud.exporter.model.MCloudModel
+import de.ingrid.igeserver.services.DocumentService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+import java.io.IOException
+import java.io.StringWriter
+import java.io.Writer
+
+@Service
+@Profile("mcloud")
+class PortalExporter : IgeExporter {
+
+    @Autowired
+    private lateinit var documentService: DocumentService
+
+    override fun getTypeInfo(): ExportTypeInfo {
+        return ExportTypeInfo(
+                "portal",
+                "mCLOUD Portal",
+                "Export der Daten für die weitere Verwendung im Liferay Portal und Exporter.")
+    }
+
+    @Throws(IOException::class)
+    override fun run(jsonData: JsonNode): Any {
+        val engine = PebbleEngine.Builder()
+                .newLineTrimming(false)
+                .build()
+
+        val compiledTemplate = engine.getTemplate("templates/export/mcloud/portal.peb")
+
+        val writer: Writer = StringWriter()
+        val map = getMapFromObject(jsonData)
+        compiledTemplate.evaluate(writer, map)
+        return writer.toString().replace("\\s+\n".toRegex(), "\n")
+    }
+
+    override fun toString(exportedObject: Any): String {
+        return exportedObject.toString()
+    }
+
+    private fun getMapFromObject(json: JsonNode): Map<String, Any> {
+
+        return mapOf( "model" to jacksonObjectMapper().convertValue(json, MCloudModel::class.java))
+
+    }
+}
