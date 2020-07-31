58f7e21b97261d357720aab375278cefd0c191f0
André Wallat <andre.wallat@wemove.com>
2020-07-31 12:18:18 +0200

    Add index page to frontend to start indexing

---
+++ frontend/src/app/+catalog/indexing/index.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ConfigService, Configuration} from '../../services/config/config.service';
+import {Catalog} from '../services/catalog.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IndexService {
+
+  private configuration: Configuration;
+  private catalog: Catalog;
+
+  constructor(private http: HttpClient, configService: ConfigService) {
+    this.configuration = configService.getConfiguration();
+    this.catalog = configService.$userInfo.getValue().currentCatalog;
+  }
+
+  start() {
+    return this.http.post(this.configuration.backendUrl + 'index', {
+      catalogId: this.catalog.id,
+      format: 'portal'
+    });
+  }
+}
---
+++ frontend/src/app/+catalog/indexing/indexing.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {IndexService} from './index.service';
+
+@Component({
+  selector: 'ige-indexing',
+  templateUrl: './indexing.component.html',
+  styleUrls: ['./indexing.component.scss']
+})
+export class IndexingComponent implements OnInit {
+
+  constructor(private indexService: IndexService) { }
+
+  ngOnInit(): void {
+  }
+
+  index() {
+    this.indexService.start().subscribe()
+  }
+}
