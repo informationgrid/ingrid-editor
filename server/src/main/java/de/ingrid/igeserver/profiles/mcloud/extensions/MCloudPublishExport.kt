d808c9f16c0941b69bf9407e177eaac1a08f1614
André Wallat <andre.wallat@wemove.com>
2020-10-07 10:55:01 +0200

    Initialize mcloud-publish bean only when profile mcloud AND elasticsearch are active

--- server/src/main/java/de/ingrid/igeserver/profiles/mcloud/extensions/MCloudPublishExport.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/mcloud/extensions/MCloudPublishExport.kt
@@ -14,9 +14,11 @@ import de.ingrid.utils.ElasticDocument
 import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
+@Profile("mcloud & elasticsearch")
 class MCloudPublishExport : Filter<PostPublishPayload> {
 
     val log = logger()
