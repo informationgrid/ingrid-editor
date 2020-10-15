informationgrid/ingrid-editor

commit 0e4267b4ea12c19dec2dcbddd01b68d525a364ae

Fix bug #2180 (deleting a document with multiple versions caused an exception)
Make sure that method getByDocumentId is only called for document wrappers

server/src/main/java/de/ingrid/igeserver/services/DocumentService.kt
@@ -12,15 +12,12 @@ import de.ingrid.igeserver.persistence.filter.*
 import de.ingrid.igeserver.persistence.model.EntityType
 import de.ingrid.igeserver.persistence.model.document.DocumentType
 import de.ingrid.igeserver.persistence.model.document.DocumentWrapperType
-import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import kotlin.reflect.KClass
 
 @Service
 class DocumentService : MapperService() {
-
-    private val log = logger()
 
     @Autowired
     private lateinit var documentTypes: List<EntityType>
@@ -60,8 +57,12 @@ class DocumentService : MapperService() {
         DRAFT("W")
     }
 
-    fun <T : EntityType> getByDocumentId(id: String, type: KClass<T>, withReferences: Boolean, ignoreMultipleCheck: Boolean = false): JsonNode? {
+    /**
+     * Get the DocumentWrapper with the given document uuid
+     */
+    fun getWrapperByDocumentId(id: String, withReferences: Boolean): JsonNode? {
 
+        val type = DocumentWrapperType::class
         val query = listOf(QueryField(FIELD_ID, id))
         val findOptions = FindOptions(
                 queryType = QueryType.EXACT,
@@ -70,13 +71,7 @@ class DocumentService : MapperService() {
         return when (docs.totalHits) {
             0L -> throw NotFoundException.withMissingResource(id, type.simpleName)
             1L -> docs.hits[0]
-            else -> {
-                if (ignoreMultipleCheck) {
-                    docs.hits[0]
-                } else {
-                    throw PersistenceException.withMultipleEntities(id, type.simpleName, dbService.currentDatabase)
-                }
-            }
+            else -> throw PersistenceException.withMultipleEntities(id, type.simpleName, dbService.currentDatabase)
         }
     }
 
@@ -105,11 +100,16 @@ class DocumentService : MapperService() {
         return dbService.findAll(DocumentWrapperType::class, queryMap, findOptions)
     }
 
-    fun getLatestDocument(doc: JsonNode, onlyPublished: Boolean = false, resolveLinks: Boolean = true): ObjectNode {
+    /**
+     * Get the latest version of the document associated with the given wrapper
+     * If onlyPublished is true, the method will return the published version and will throw an exception,
+     * if it does not exist. If onlyPublished is false, it will prefer the draft version, if it exists.
+     */
+    fun getLatestDocument(wrapper: JsonNode, onlyPublished: Boolean = false, resolveLinks: Boolean = true): ObjectNode {
 
-        val docData: ObjectNode = getDocumentVersion(doc, onlyPublished)
+        val docData: ObjectNode = getLatestDocumentVersion(wrapper, onlyPublished)
 
-        return prepareDocument(docData, doc[FIELD_DOCUMENT_TYPE].asText(), onlyPublished, resolveLinks)
+        return prepareDocument(docData, wrapper[FIELD_DOCUMENT_TYPE].asText(), onlyPublished, resolveLinks)
     }
 
     fun getDocumentType(docType: String): EntityType {
@@ -151,7 +151,7 @@ class DocumentService : MapperService() {
         val docType = getDocumentType(docTypeName)
 
         // run pre-update pipe(s)
-        val wrapper = getByDocumentId(id, DocumentWrapperType::class, false)
+        val wrapper = getWrapperByDocumentId(id, false)
         val preUpdatePayload = PreUpdatePayload(docType, data as ObjectNode, wrapper as ObjectNode)
         preUpdatePipe.runFilters(preUpdatePayload, filterContext)
         if (publish) {
@@ -188,7 +188,9 @@ class DocumentService : MapperService() {
                 if (draftId.isNull) {
                     // TODO: db_id is ORecord!
                     put(FIELD_DRAFT, updatedRecordId)
-                } else {
+                }
+                else {
+                    // 'if' must have both main and 'else' branches if used as an expression
                 }
             }
         }
@@ -219,13 +221,13 @@ class DocumentService : MapperService() {
         val filterContext = DefaultContext.withCurrentProfile(dbService)
 
         // run pre-delete pipe(s)
-        val data = getByDocumentId(id, DocumentType::class, false, true)
-        if (data != null) {
+        val wrapper = getWrapperByDocumentId(id, false)
+        if (wrapper != null) {
+            val data = getLatestDocumentVersion(wrapper, false)
             val docTypeName = data.get(FIELD_DOCUMENT_TYPE).asText()
             val docType = getDocumentType(docTypeName)
 
-            val wrapper = getByDocumentId(id, DocumentWrapperType::class, true)
-            val preDeletePayload = PreDeletePayload(docType, data as ObjectNode, wrapper as ObjectNode)
+            val preDeletePayload = PreDeletePayload(docType, data, wrapper as ObjectNode)
             preDeletePipe.runFilters(preDeletePayload, filterContext)
 
             findChildrenDocs(id, isAddress(wrapper)).hits.forEach {
@@ -248,7 +250,7 @@ class DocumentService : MapperService() {
     fun revertDocument(id: String): JsonNode {
 
         // remove draft version
-        val wrapper = getByDocumentId(id, DocumentWrapperType::class, false) as ObjectNode
+        val wrapper = getWrapperByDocumentId(id, false) as ObjectNode
 
         val publishedId = wrapper.get(FIELD_PUBLISHED)
 
@@ -322,12 +324,12 @@ class DocumentService : MapperService() {
                 .category
     }
 
-    private fun getDocumentVersion(doc: JsonNode, onlyPublished: Boolean): ObjectNode {
-        val draft = doc[FIELD_DRAFT]
-        val published = doc[FIELD_PUBLISHED]
+    private fun getLatestDocumentVersion(wrapper: JsonNode, onlyPublished: Boolean): ObjectNode {
+        val draft = wrapper[FIELD_DRAFT]
+        val published = wrapper[FIELD_PUBLISHED]
 
         if (onlyPublished && published.isNull) {
-            throw NotFoundException.withMissingPublishedVersion(doc.get(FIELD_ID).asText())
+            throw NotFoundException.withMissingPublishedVersion(wrapper.get(FIELD_ID).asText())
         }
 
         // TODO: check if isNull function really works or if we need a null comparison
@@ -337,7 +339,7 @@ class DocumentService : MapperService() {
             draft as ObjectNode
         }
 
-        objectNode.put(FIELD_STATE, if (onlyPublished) DocumentState.PUBLISHED.value else determineState(doc))
+        objectNode.put(FIELD_STATE, if (onlyPublished) DocumentState.PUBLISHED.value else determineState(wrapper))
 
         return objectNode
     }

server/src/main/java/de/ingrid/igeserver/api/DatasetsApiController.kt
@@ -104,7 +104,7 @@ class DatasetsApiController @Autowired constructor(private val authUtils: AuthUt
 
     private fun handleCopy(id: String, options: CopyOptions): JsonNode {
 
-        val wrapper = documentService.getByDocumentId(id, DocumentWrapperType::class, true) as ObjectNode
+        val wrapper = documentService.getWrapperByDocumentId(id, true) as ObjectNode
 
         val doc = documentService.getLatestDocument(wrapper, false, false)
 
@@ -143,7 +143,7 @@ class DatasetsApiController @Autowired constructor(private val authUtils: AuthUt
         docs.hits.forEach { doc: JsonNode ->
 
             val id = doc.get(FIELD_ID).asText()
-            val child = documentService.getByDocumentId(id, DocumentWrapperType::class, true)
+            val child = documentService.getWrapperByDocumentId(id, true)
             child?.let {
                 val childVersion = documentService.getLatestDocument(it, false, false)
                 childVersion.put(FIELD_PARENT, parentId)
@@ -156,7 +156,7 @@ class DatasetsApiController @Autowired constructor(private val authUtils: AuthUt
 
     private fun handleMove(id: String, options: CopyOptions) {
 
-        val wrapper = documentService.getByDocumentId(id, DocumentWrapperType::class, true) as ObjectNode
+        val wrapper = documentService.getWrapperByDocumentId(id, true) as ObjectNode
         val doc = documentService.getLatestDocument(wrapper, false, true)
 
         if (id == options.destId) {
@@ -173,7 +173,7 @@ class DatasetsApiController @Autowired constructor(private val authUtils: AuthUt
         documentService.updateDocument(id, doc, published)
 
         // updateWrapper
-        val wrapperWithLinks = documentService.getByDocumentId(id, DocumentWrapperType::class, false) as ObjectNode
+        val wrapperWithLinks = documentService.getWrapperByDocumentId(id, false) as ObjectNode
         wrapperWithLinks.put(FIELD_PARENT, options.destId)
         val wrapperId = dbService.getRecordId(wrapperWithLinks)
         dbService.save(DocumentWrapperType::class, wrapperId, wrapperWithLinks.toString())
@@ -283,7 +283,7 @@ class DatasetsApiController @Autowired constructor(private val authUtils: AuthUt
 
         dbService.acquire(dbId).use {
             while (true) {
-                val doc = documentService.getByDocumentId(parentId, DocumentWrapperType::class, false) ?: break
+                val doc = documentService.getWrapperByDocumentId(parentId, false) ?: break
                 val nextParentId = doc[FIELD_PARENT]?.textValue()
                 if (nextParentId != null) {
                     path.add(nextParentId)

server/src/main/java/de/ingrid/igeserver/api/ExportApiController.kt
@@ -3,7 +3,6 @@ package de.ingrid.igeserver.api
 import de.ingrid.igeserver.exports.ExportTypeInfo
 import de.ingrid.igeserver.model.ExportRequestParameter
 import de.ingrid.igeserver.persistence.DBApi
-import de.ingrid.igeserver.persistence.model.document.DocumentWrapperType
 import de.ingrid.igeserver.services.CatalogService
 import de.ingrid.igeserver.services.DocumentService
 import de.ingrid.igeserver.services.ExportService
@@ -35,7 +34,7 @@ class ExportApiController : ExportApi {
         // TODO: option to export addresses too?
         var result: String? = ""
         dbService.acquire(dbId).use {
-            val doc = documentService.getByDocumentId(data.id, DocumentWrapperType::class, true)
+            val doc = documentService.getWrapperByDocumentId(data.id, true)
             if (doc != null) {
                 val docVersion = documentService.getLatestDocument(doc, !data.isUseDraft)
 

server/src/main/java/de/ingrid/igeserver/imports/ImportService.kt
@@ -3,7 +3,6 @@ package de.ingrid.igeserver.imports
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import de.ingrid.igeserver.persistence.DBApi
-import de.ingrid.igeserver.persistence.model.document.DocumentType
 import de.ingrid.igeserver.services.DocumentService
 import de.ingrid.igeserver.services.FIELD_DOCUMENT_TYPE
 import de.ingrid.igeserver.services.FIELD_ID
@@ -74,12 +73,11 @@ class ImportService {
         // TODO: optimize by caching reference information
 
         val id = ref.path(FIELD_ID).textValue()
-        try {
-            // TODO: use DocumentWrapper instead?
-            documentService.getByDocumentId(id, DocumentType::class, false, true)
-            return true
+        return try {
+            documentService.getWrapperByDocumentId(id, false)
+            true
         } catch (e: RuntimeException) {
-            return false
+            false
         }
 
     }

server/src/main/java/de/ingrid/igeserver/migrations/tasks/M018_FixParents.kt
@@ -38,7 +38,7 @@ class M018_FixParents : MigrationBase("0.18") {
                     .forEach {
                         count++
                         try {
-                            val wrapper = docService.getByDocumentId(it.get(FIELD_ID).asText(), DocumentWrapperType::class, false)
+                            val wrapper = docService.getWrapperByDocumentId(it.get(FIELD_ID).asText(), false)
                             if (wrapper != null) {
                                 val wrapperParent = wrapper.get(FIELD_PARENT)
                                 if (wrapperParent != null && !wrapperParent.asText().isEmpty() && wrapperParent.asText() != "null") {

server/src/main/java/de/ingrid/igeserver/profiles/mcloud/types/impl/OMCloudType.kt
@@ -32,7 +32,7 @@ class OMCloudType : BaseMCloudType(), OrientDBDocumentEntityType {
         val addresses = doc.path("addresses")
         for (address in addresses) {
             val wrapperId = address.path("ref").asText()
-            val wrapper = docService.getByDocumentId(wrapperId, DocumentWrapperType::class, true)
+            val wrapper = docService.getWrapperByDocumentId(wrapperId, true)
                     ?: throw NotFoundException.withMissingResource(wrapperId, DocumentWrapperType::class.simpleName)
             try {
                     // try to find the published version of the linked document
@@ -71,7 +71,7 @@ class OMCloudType : BaseMCloudType(), OrientDBDocumentEntityType {
         for (address in addresses) {
             val wrapperId = address.path("ref").asText()
             try {
-                val wrapper = docService.getByDocumentId(wrapperId, DocumentWrapperType::class, true)
+                val wrapper = docService.getWrapperByDocumentId(wrapperId, true)
                 if (wrapper != null) {
                     val latestDocument = docService.getLatestDocument(wrapper, onlyPublished)
                     (address as ObjectNode).replace("ref", latestDocument)

commit 0a0bd68dddef75000955386e390fbf967fb1034c

Fix error introduced in 0e4267b4ea12c19dec2dcbddd01b68d525a364ae (deleting documents)

server/src/main/java/de/ingrid/igeserver/services/DocumentService.kt
@@ -221,7 +221,7 @@ class DocumentService : MapperService() {
         val filterContext = DefaultContext.withCurrentProfile(dbService)
 
         // run pre-delete pipe(s)
-        val wrapper = getWrapperByDocumentId(id, false)
+        val wrapper = getWrapperByDocumentId(id, true)
         if (wrapper != null) {
             val data = getLatestDocumentVersion(wrapper, false)
             val docTypeName = data.get(FIELD_DOCUMENT_TYPE).asText()