1b9258ee5373cecd0d3d823cba5e4f209b098b37
André Wallat <andre.wallat@wemove.com>
2020-11-19 15:30:41 +0100

    Adapt HMR configuration for Angular 11

--- frontend/src/environments/environment.hmr.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  hmr: true,
-  profileFromServer: false,
-  mockKeycloak: true,
-  configFile: 'assets/config.json'
-};
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
-import {hmrBootstrap} from './hmr';
 import {enableAkitaProdMode, persistState} from '@datorama/akita';
 
 // add scrollto polyfill for ie11
@@ -20,15 +19,4 @@ persistState({
   preStorageUpdate: (storeName: string, state: any) => ({ui: state.ui, recentAddresses: state.recentAddresses})
 });
 
-const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
-
-if (environment.hmr) {
-  if (module[ 'hot' ]) {
-    hmrBootstrap(module, bootstrap);
-  } else {
-    console.error('HMR is not enabled for webpack-dev-server!');
-    console.log('Are you using the --hmr flag for ng serve?');
-  }
-} else {
-  bootstrap();
-}
+platformBrowserDynamic().bootstrapModule(AppModule);
