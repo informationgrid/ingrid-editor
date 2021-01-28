029a43fa2ab7387f2e8e151183bc6ef46484af2b
André Wallat <andre.wallat@wemove.com>
2021-01-28 09:53:29 +0100

    Tests: Set tests to pending

--- frontend/e2e/cypress/integration/catalog/manage.spec.ts
+++ frontend/e2e/cypress/integration/catalog/manage.spec.ts
@@ -1,17 +1,17 @@
 describe('Catalog management', () => {
-  it('should create a new catalog', () => {});
+  xit('should create a new catalog', () => {});
 
-  it('should modify an existing dialog', () => {});
+  xit('should modify an existing dialog', () => {});
 
-  it('should delete an existing dialog', () => {
+  xit('should delete an existing dialog', () => {
     // deletion should only work by typing word "Löschen"
     // test with wrong word
     // test with correct word -> real delete
   });
 
-  it('should add a catalog administrator', () => {});
+  xit('should add a catalog administrator', () => {});
 
-  it('should switch between two catalogs', () => {
+  xit('should switch between two catalogs', () => {
     // use "Verwenden" button on catalog to switch to new catalog
     // check if dataset are different
     // check if search only gets documents from the correct catalog
