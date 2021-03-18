8500e18983cb38ffea165cb4dae2c1fe0e46d7bc
iherwig <ingo@wemove.com>
2021-03-18 09:57:25 +0100

    Add CatalogManagerAssignment entity, change UserInfo.userId to var for testing

---
+++ server/src/main/java/de/ingrid/igeserver/persistence/postgresql/jpa/model/ige/CatalogManagerAssignment.kt
@@ -0,0 +1,48 @@
+package de.ingrid.igeserver.persistence.postgresql.jpa.model.ige
+
+import com.fasterxml.jackson.annotation.*
+import de.ingrid.igeserver.annotations.NoArgs
+import de.ingrid.igeserver.persistence.postgresql.jpa.model.EntityWithCatalog
+import javax.persistence.*
+import java.io.Serializable
+
+// https://stackoverflow.com/questions/38934436/join-3-tables-hibernate-jpa
+
+@Embeddable
+class AssignmentKey : Serializable {
+    @Column(name="catalog_id")
+    var catalogId: Int = 0
+
+    @Column(name="manager_id")
+    var managerId: Int = 0
+
+    @Column(name="user_id")
+    var userId: Int = 0
+}
+
+@NoArgs
+@Entity
+@Table(name="manager")
+class CatalogManagerAssignment : EntityWithCatalog  {
+
+    @EmbeddedId
+    var id: AssignmentKey? = null
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @MapsId("catalogId")
+    @JoinColumn(name="catalog_id", nullable=false)
+    @JsonIgnore
+    override var catalog: Catalog? = null
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @MapsId("managerId")
+    @JoinColumn(name="manager_id", nullable=false)
+    @JsonIgnore
+    var manager: UserInfo? = null
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @MapsId("userId")
+    @JoinColumn(name="user_id", nullable=false)
+    @JsonIgnore
+    var user: UserInfo? = null
+}
