a4d771bedef0b27cc2034bbd1e51f9dbf9af4a06
André Wallat <andre.wallat@wemove.com>
2021-03-19 16:14:45 +0100

    Fix test

--- frontend/src/app/+settings/catalog-management/catalog-management.component.spec.ts
+++ frontend/src/app/+settings/catalog-management/catalog-management.component.spec.ts
@@ -1,25 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CatalogManagementComponent } from './catalog-management.component';
+import {CatalogManagementComponent} from './catalog-management.component';
+import {createComponentFactory, mockProvider, Spectator} from '@ngneat/spectator';
+import {CodelistService} from '../../services/codelist/codelist.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatDialogModule} from '@angular/material/dialog';
+import {CatalogService} from '../../+catalog/services/catalog.service';
+import {of} from 'rxjs';
+import {AddButtonModule} from '../../shared/add-button/add-button.module';
 
 describe('CatalogManagementComponent', () => {
-  let component: CatalogManagementComponent;
-  let fixture: ComponentFixture<CatalogManagementComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CatalogManagementComponent ]
-    })
-    .compileComponents();
+  let spectator: Spectator<CatalogManagementComponent>;
+  const createHost = createComponentFactory({
+    component: CatalogManagementComponent,
+    imports: [RouterTestingModule, HttpClientTestingModule, MatDialogModule, AddButtonModule],
+    providers: [
+      mockProvider(CodelistService),
+      mockProvider(CatalogService, {
+        getCatalogProfiles: () => of([])
+      })
+    ],
+    mocks: [],
+    detectChanges: false
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CatalogManagementComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    spectator = createHost();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 });
