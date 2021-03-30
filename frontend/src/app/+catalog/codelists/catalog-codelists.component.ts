7f4fb805ab5e87425e8de9343be2860257091428
André Wallat <andre.wallat@wemove.com>
2021-03-30 17:26:41 +0200

    Improve style of catalog codelists

--- frontend/src/app/+catalog/codelists/catalog-codelists.component.ts
+++ frontend/src/app/+catalog/codelists/catalog-codelists.component.ts
@@ -6,6 +6,7 @@ import {catchError, map} from 'rxjs/operators';
 import {CodelistQuery} from '../../store/codelist/codelist.query';
 import {MatDialog} from '@angular/material/dialog';
 import {UpdateCodelistComponent} from './update-codelist/update-codelist.component';
+import {ConfirmDialogComponent, ConfirmDialogData} from '../../dialogs/confirm/confirm-dialog.component';
 
 @Component({
   selector: 'ige-catalog-codelists',
@@ -66,10 +67,32 @@ export class CatalogCodelistsComponent implements OnInit {
       minWidth: 300,
       data: entry
     }).afterClosed().subscribe(result => {
+      if (!result) return;
       const index = this.selectedCodelist[0].entries
         .findIndex(e => e.id === oldId);
       const other = JSON.parse(JSON.stringify(this.selectedCodelist[0]));
       other.entries.splice(index, 1, result);
+      this.codelistService.updateCodelist(other);
+    });
+  }
+
+  removeCodelist(entry: CodelistEntry) {
+    this.dialog.open(ConfirmDialogComponent, {
+      data: <ConfirmDialogData>{
+        message: `Möchten Sie den Codelist-Eintrag "${entry.value}" wirklich löschen:`,
+        title: 'Löschen',
+        buttons: [
+          {text: 'Abbrechen'},
+          {text: 'Löschen', alignRight: true, id: 'confirm', emphasize: true}
+        ]
+      }
+    }).afterClosed().subscribe(result => {
+      if (!result) return;
+      const oldId = entry.id;
+      const index = this.selectedCodelist[0].entries
+        .findIndex(e => e.id === oldId);
+      const other = JSON.parse(JSON.stringify(this.selectedCodelist[0]));
+      other.entries.splice(index, 1);
       this.codelistService.updateCodelist(other);
     });
   }
