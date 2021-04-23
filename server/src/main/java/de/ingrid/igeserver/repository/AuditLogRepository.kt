6db389b55465b8eebda3b4e8442de75d46666a97
André Wallat <andre.wallat@wemove.com>
2021-04-23 17:34:47 +0200

    Add Repo function

--- server/src/main/java/de/ingrid/igeserver/repository/AuditLogRepository.kt
+++ server/src/main/java/de/ingrid/igeserver/repository/AuditLogRepository.kt
@@ -5,4 +5,6 @@ import de.ingrid.igeserver.persistence.postgresql.jpa.model.ige.VersionInfo
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AuditLogRepository : JpaRepository<AuditLogRecord, Int> {
+    
+    fun findAllByLoggerAndData_Target(logger: String, data_target: String): List<AuditLogRecord>
 }
