2979d94baf6fcc6b45b19c96ffb75549468ded1a
André Wallat <andre.wallat@wemove.com>
2021-05-10 16:46:02 +0200

    Remove db index on data field
    When saving a document with a long description it could happen that
    it could not be stored because of the index function that is executed
    on the whole form. This index should not be needed nor useful.

---
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M028_RemoveDataIndex.kt
@@ -0,0 +1,32 @@
+package de.ingrid.igeserver.migrations.tasks
+
+import de.ingrid.igeserver.migrations.MigrationBase
+import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
+import org.apache.logging.log4j.kotlin.logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
+import javax.persistence.EntityManager
+
+@Service
+class M028_RemoveDataIndex : MigrationBase("0.28") {
+
+    private var log = logger()
+
+    @Autowired
+    lateinit var entityManager: EntityManager
+
+    @Autowired
+    private lateinit var transactionManager: PlatformTransactionManager
+
+    private val sql = """
+        DROP INDEX IF EXISTS idx_document_data;
+    """.trimIndent()
+
+    override fun exec() {
+        ClosableTransaction(transactionManager).use {
+            entityManager.createNativeQuery(sql).executeUpdate()
+        }
+    }
+
+}
