eb0187d152a9bf0eddd6dda5ec60c21b1b33a2b7
André Wallat <andre.wallat@wemove.com>
2021-05-18 12:44:22 +0200

    Fix test error because of styling issue
    The divider after each section had margin-bottom set, which lead to a jumping issue
    in electron browser and test could fail.

--- frontend/e2e/cypress/pages/enterMcloudDocTestData.ts
+++ frontend/e2e/cypress/pages/enterMcloudDocTestData.ts
@@ -149,10 +149,8 @@ export class enterMcloudDocTestData {
   // *****************************************************
 
   static setTimeReference(date: Date = new Date(2020, 1, 11), choose: string = 'Erstellung') {
-    cy.get('[data-cy="Zeitbezug der Ressource"] > ige-repeat > ige-add-button').contains('Hinzufügen').click();
-    // wait for 10 seconds and check if 'Hinzufügen'-Button is clicked
-    cy.get('[data-cy="Zeitbezug der Ressource"] [svgicon=Entfernen]', { timeout: 10000 }).should('be.visible');
-    cy.get('[data-cy="Zeitbezug der Ressource"] ige-repeat mat-form-field').contains('Typ').click({force: true});
+    cy.get('[data-cy="Zeitbezug der Ressource"] ige-add-button button').click();
+    cy.get('[data-cy="Zeitbezug der Ressource"] ige-repeat mat-form-field').contains('Typ').click({ force: true });
     cy.get('.mat-option-text').contains(choose).click();
     this.selectDate('[data-cy="Zeitbezug der Ressource"]', date, choose);
   }
