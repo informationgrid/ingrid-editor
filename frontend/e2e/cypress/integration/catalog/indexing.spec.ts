4d87a116e8cedcd612094dbeb1bfab91d61f6e32
Majid Ercan <majid.ercan@wemove.com>
2021-06-21 17:29:16 +0200

    Test: add some indexing tests

--- frontend/e2e/cypress/integration/catalog/indexing.spec.ts
+++ frontend/e2e/cypress/integration/catalog/indexing.spec.ts
@@ -1,5 +1,7 @@
 import { BehavioursPage } from '../../pages/behaviours.page';
 import { CatalogsTabmenu } from '../../pages/base.page';
+import {AddressPage} from "../../pages/address.page";
+import {DocumentPage} from "../../pages/document.page";
 
 describe('Indexing', () => {
   beforeEach(() => {
@@ -11,9 +13,70 @@ describe('Indexing', () => {
     cy.kcLogout();
   });
 
-  xit('Index and compare the start- and end-time', () => {
-    // BehavioursPage.openCatalogSettingsTab(CatalogsTabmenu.Indizierung);
+  it('Index and check indexed object counter has increased', () => {
+    const json = {
+      firstName: 'vor',
+      lastName: 'nach',
+      organization: 'org',
+      title: 'APICall-forINDEX',
+      _type: 'AddressDoc',
+      contact: [{ type: 1, connection: '0123456789' }]
+    };
+
+    AddressPage.apiCreateAddress(json, true);
+
+    BehavioursPage.openCatalogSettingsTab(CatalogsTabmenu.Indizierung);
+    BehavioursPage.openIndexStatusBox();
+
+    cy.get('button').contains('Indizieren').click();
+    DocumentPage.waitUntilElementIsVisible("div.status:contains('Endzeit')");
+
+    cy.get('div.status').should('not.contain', 'Anzahl Adressen: 0');
+
+  })
+
+  it('Set a cron expression and check', () => {
+    const cron = '*/30 * * * * *';
+    const json = {
+      firstName: 'vor',
+      lastName: 'nach',
+      organization: 'org',
+      title: 'APICall-forcronINDEX',
+      _type: 'AddressDoc',
+      contact: [{ type: 1, connection: '0123456789' }]
+    };
+
+    AddressPage.apiCreateAddress(json, true);
+
+    BehavioursPage.openCatalogSettingsTab(CatalogsTabmenu.Indizierung);
+    BehavioursPage.openIndexStatusBox();
+    cy.get('mat-form-field input').clear().type(cron);
+
+    cy.get('mat-hint').contains('Alle 30 Sekunden');
+
+    cy.get('button').contains('Übernehmen').click();
+    DocumentPage.waitUntilElementIsVisible("div.status:contains('Endzeit')");
+
+    cy.get('button').contains('Keine zeitliche Indizierung').click();
   });
 
-  xit('Set a cron expression and check', () => {});
+  it('Set cron expression via example buttons', () => {
+    const json = {
+      firstName: 'vor',
+      lastName: 'nach',
+      organization: 'org',
+      title: 'APICall-forcronexampleINDEX',
+      _type: 'AddressDoc',
+      contact: [{ type: 1, connection: '0123456789' }]
+    };
+
+    AddressPage.apiCreateAddress(json, true);
+
+    BehavioursPage.openCatalogSettingsTab(CatalogsTabmenu.Indizierung);
+
+    cy.get('mat-chip').contains('Alle 30 Minuten').click();
+
+    cy.get('mat-hint').contains('Alle 30 Minuten');
+  });
+
 });
