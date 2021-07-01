3766a33b7ec4168c2347f8289dccc4ef599363af
André Wallat <andre.wallat@wemove.com>
2021-07-01 11:42:40 +0200

    Fix deletion of group
    A group where the currently logged in user belongs to, could not
    be deleted by hibernate. By using a separate query-definition deletion
    now works for all groups.

--- server/src/main/java/de/ingrid/igeserver/repository/GroupRepository.kt
+++ server/src/main/java/de/ingrid/igeserver/repository/GroupRepository.kt
@@ -3,11 +3,21 @@ package de.ingrid.igeserver.repository
 import de.ingrid.igeserver.persistence.postgresql.jpa.model.ige.Group
 import org.springframework.data.domain.Sort
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface GroupRepository : JpaRepository<Group, Int> {
     
     fun findAllByCatalog_Identifier(catalog_identifier: String, sort: Sort = Sort.by(Sort.Direction.ASC, "name")): List<Group>
     
     fun findAllByCatalog_IdentifierAndId(catalog_identifier: String, id: Int): Group
+
+    /**
+     * This function needs to be overridden, otherwise a group is not deleted if the
+     * currently logged in user belongs to this group.
+     */
+    @Modifying
+    @Query("delete from Group g where g.id = ?1")
+    override fun deleteById(id: Int)
     
 }
