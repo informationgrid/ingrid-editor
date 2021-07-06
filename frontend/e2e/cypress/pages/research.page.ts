f1130082c3eb51cf8bdcccc4f236963dc29b7f21
Lisa Karolina Raschke <lisa.karolina.raschke@wemove.com>
2021-07-06 18:30:30 +0200

    Test: added function to switch search filter on Research page

--- frontend/e2e/cypress/pages/research.page.ts
+++ frontend/e2e/cypress/pages/research.page.ts
@@ -1,4 +1,5 @@
 import Chainable = Cypress.Chainable;
+import { Subject } from 'rxjs';
 
 export class ResearchPage {
   static url = '/research';
@@ -13,10 +14,17 @@ export class ResearchPage {
   static getSearchResultCount(): Chainable<number> {
     return cy
       .get('.result')
-      .contains(/[1-9][0-9]* Ergebnisse gefunden/ )
+      .contains(/[1-9][0-9]* Ergebnisse gefunden/)
       .then($node => {
         // extract number from string like '12 Ergebnisse gefunden'
         return parseInt($node.text().split(' ')[0]);
       });
+  }
+
+  static toggleSearchFilter(docType: string): Chainable<any> {
+    cy.get('.main-header .mat-select').click();
+    return docType === 'Adressen'
+      ? cy.get('.mat-option-text').contains('Adressen').click()
+      : cy.get('.mat-option-test').contains('Dokumente').click();
   }
 }
