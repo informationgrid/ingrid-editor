bb8c27555bbbc8eebedf74483bdd2472294b6a29
Lisa Karolina Raschke <lisa.karolina.raschke@wemove.com>
2021-07-20 19:05:21 +0200

    add tests for research page

--- frontend/e2e/cypress/integration/research/search.spec.ts
+++ frontend/e2e/cypress/integration/research/search.spec.ts
@@ -1,5 +1,10 @@
-import { BasePage } from '../../pages/base.page';
-import { ResearchPage } from '../../pages/research.page';
+import {
+  contextActionSearchResult,
+  FilterExtendedSearch,
+  ResearchPage,
+  SearchOptionTabs
+} from '../../pages/research.page';
+import { DocumentPage } from '../../pages/document.page';
 
 describe('Research Page', () => {
   beforeEach(() => {
@@ -12,44 +17,207 @@ describe('Research Page', () => {
   });
 
   //Erweiterte Suche
-  xit('should do general search with without-folders-filter for documents/addresses', () => {});
+  it('should do general search with without-folders-filter for documents/addresses', () => {
+    ResearchPage.visit();
+    ResearchPage.search('a');
+    ResearchPage.getSearchResultCount().then(allCount => {
+      //wait for the filtered result in order to compare
+      cy.intercept({
+        method: 'POST',
+        path: '/api/search/query'
+      }).as('filterRequest');
+      ResearchPage.checkboxSearchFilter(FilterExtendedSearch.NoFolders);
+      cy.wait('@filterRequest');
+      ResearchPage.getSearchResultCount().should('be.lessThan', allCount).and('be.greaterThan', 0);
+    });
+  });
 
-  xit('should do general search with only-published-filter for documents/addresses', () => {});
+  it('should do general search with only-published-filter for documents/addresses', () => {
+    ResearchPage.visit();
+    ResearchPage.search('a');
+    ResearchPage.getSearchResultCount().then(allCount => {
+      //wait for the filtered result in order to compare
+      cy.intercept({
+        method: 'POST',
+        path: '/api/search/query'
+      }).as('filterRequest');
+      ResearchPage.checkboxSearchFilter(FilterExtendedSearch.OnlyPublished);
+      cy.wait('@filterRequest');
+      ResearchPage.getSearchResultCount().should('be.lessThan', allCount).and('be.greaterThan', 0);
+    });
+  });
 
-  xit('should do document type search with with only-mCloud-filter', () => {});
+  it('should do document type search with with only-mCloud-filter', () => {
+    ResearchPage.visit();
+    ResearchPage.search('a');
+    ResearchPage.getSearchResultCount().then(allCount => {
+      //wait for the filtered result in order to compare
+      cy.intercept({
+        method: 'POST',
+        path: '/api/search/query'
+      }).as('filterRequest');
+      ResearchPage.checkboxSearchFilter(FilterExtendedSearch.mCloud);
+      cy.wait('@filterRequest');
+      ResearchPage.getSearchResultCount().should('be.lessThan', allCount).and('be.greaterThan', 0);
+    });
+  });
 
-  xit('should do document type search with with only-Test-filter', () => {});
+  xit('should do document type search with only-Test-filter', () => {
+    ResearchPage.visit();
+    ResearchPage.search('er');
+    ResearchPage.getSearchResultCount().then(allCount => {
+      //wait for the filtered result in order to compare
+      cy.intercept({
+        method: 'POST',
+        path: '/api/search/query'
+      }).as('filterRequest');
+      ResearchPage.checkboxSearchFilter(FilterExtendedSearch.Test);
+      cy.wait('@filterRequest');
+      ResearchPage.getSearchResultCount().should('be.lessThan', allCount).and('be.greaterThan', 0);
+    });
+  });
 
-  xit('should search for documents with spatial reference', () => {});
+  xit('should search for documents with spatial reference', () => {
+    ResearchPage.visit();
+    ResearchPage.search('e');
+    ResearchPage.getSearchResultCount().then(allCount => {
+      //wait for the filtered result in order to compare
+      cy.intercept({
+        method: 'POST',
+        path: '/api/search/query'
+      }).as('filterRequest');
+      ResearchPage.createSpatialReference('Deutschland', 'testSpatial1');
+      cy.wait('@filterRequest');
+      ResearchPage.getSearchResultCount().should('be.lessThan', allCount).and('be.greaterThan', 0);
+    });
+  });
 
-  xit('should start new search for documents after editing spatial reference', () => {});
+  xit('should start new search for documents after editing spatial reference', () => {
+    ResearchPage.visit();
+    ResearchPage.search('e');
+    cy.intercept({
+      method: 'POST',
+      path: '/api/search/query'
+    }).as('filterRequest');
+    ResearchPage.createSpatialReference('Deutschland');
+    cy.wait('@filterRequest');
+    ResearchPage.getSearchResultCount().then(filteredResult => {
+      cy.wait('@filterRequest');
+      ResearchPage.editSpatialReference('Berlin');
+      //Problem: new search result after editing is not necessarily smaller than the former result
+      ResearchPage.getSearchResultCount().should('be.lessThan', filteredResult).and('be.greaterThan', 0);
+    });
+  });
 
-  xit('should save particular search so that all the parameters are correctly saved under Gespeicherte Suchen', () => {});
+  it('should save particular search so that all the parameters are correctly saved under Gespeicherte Suchen', () => {
+    ResearchPage.visit();
+    ResearchPage.search('e');
+    ResearchPage.saveSearchProfile('test-50', 'save description');
+    ResearchPage.openSearchOptionTab(SearchOptionTabs.SavedSearches);
+    ResearchPage.checkExistenceOfSavedSearch('test-50');
+  });
 
-  xit('should start new search for documents after deleting spatial reference', () => {});
+  xit('should start new search for documents after deleting spatial reference', () => {
+    ResearchPage.visit();
+    ResearchPage.createSpatialReference('Mainz');
+    cy.wait(500);
+    ResearchPage.getSearchResultCount().then(spatiallyFiltered => {
+      ResearchPage.deleteSpatialReference();
+      ResearchPage.getSearchResultCount().should('be.greaterThan', spatiallyFiltered);
+    });
+  });
 
-  xit('should do search with both spatial reference and selection of filter-checkboxes included', () => {});
+  xit('should do search with both spatial reference and selection of filter-checkboxes included', () => {
+    ResearchPage.visit();
+    ResearchPage.search('e');
+    ResearchPage.checkboxSearchFilter(FilterExtendedSearch.OnlyPublished);
+    ResearchPage.createSpatialReference('Deutschland');
+    ResearchPage.getSearchResultCount().then(multipleFiltered => {
+      ResearchPage.checkboxSearchFilter(FilterExtendedSearch.OnlyPublished);
+      ResearchPage.getSearchResultCount().should('be.lessThan', multipleFiltered);
+    });
+  });
 
-  xit('should open Download-Dialogue when requesting CSV-file of search', () => {});
+  //save for later
+  xit('should open Download-Dialogue when requesting CSV-file of search', () => {
+    cy.get('ige-result-table').find('button > span:contains("CSV")').click(); //open up CSV dialogue
+  });
 
-  xit('should open respective document/address when clicking on search result', () => {});
+  it('should open respective document/address when clicking on search result', () => {
+    ResearchPage.visit();
+    ResearchPage.search('e');
+    cy.contains('td', 'testDeleting1').click();
+    cy.get('ige-header-title-row').find('span > span').should('have.text', 'testDeleting1');
+  });
 
-  xit('should delete document/address via the dialogue accessible from the search result list', () => {});
+  it('should delete document/address via the dialogue accessible from the search result list', () => {
+    DocumentPage.visit();
+    DocumentPage.createDocument('testDeleting1');
+    ResearchPage.visit();
+    ResearchPage.search('test');
+    ResearchPage.openContextMenuOfSearchResult('testDeleting1', contextActionSearchResult.Delete);
+  });
 
-  xit('should do search by using feldbezogener Suche', () => {});
+  //will fail as long as "feldbezogene Suche" is not possible
+  xit('should do search by using feldbezogene Suche', () => {
+    ResearchPage.visit();
+    ResearchPage.toggleSearchFilter('Adressen');
+    ResearchPage.search('title:Testorganisation');
+    ResearchPage.getSearchResultCount().should('be.greaterThan', 0);
+  });
 
   //if possible:
   xit('should make sure CSV-file of search has been downloaded', () => {});
 
   //SQL-Suche
-  xit('should do search with example SQL-query executed by button', () => {});
+  it('should do search with example SQL-query executed by button', () => {
+    ResearchPage.visit();
+    ResearchPage.openSearchOptionTab(SearchOptionTabs.SQLSearch);
+    cy.get('div.mat-chip-list-wrapper > mat-chip.mat-chip').eq(0).click(); //button "Adressen, mit Titel 'test'"
+    ResearchPage.getSearchResultCount().should('be.greaterThan', 0);
+  });
 
-  xit('should do search with example SQL-query typed in manually', () => {});
+  it('should do search with example SQL-query typed in manually', () => {
+    ResearchPage.visit();
+    ResearchPage.openSearchOptionTab(SearchOptionTabs.SQLSearch);
+    cy.get(ResearchPage.SQLField).type(
+      'SELECT document1.*, document_wrapper.*\n' +
+        '            FROM document_wrapper\n' +
+        '                   JOIN document document1 ON\n' +
+        '              CASE\n' +
+        '                WHEN document_wrapper.draft IS NULL THEN document_wrapper.published = document1.id\n' +
+        '                ELSE document_wrapper.draft = document1.id\n' +
+        '                END\n' +
+        "            WHERE document1.type = 'AddressDoc'\n" +
+        "              AND LOWER(title) LIKE '%test%'"
+    );
+    ResearchPage.getSearchResultCount().then(manualSQLSearchResult => {
+      cy.get('div.mat-chip-list-wrapper > mat-chip.mat-chip').eq(0).click();
+      ResearchPage.getSearchResultCount().should('equal', manualSQLSearchResult);
+    });
+  });
 
-  xit('should delete SQL-query and subsequently return 0 results', () => {});
+  it('should delete SQL-query and subsequently return 0 results', () => {
+    ResearchPage.visit();
+    ResearchPage.openSearchOptionTab(SearchOptionTabs.SQLSearch);
+    cy.get('div.mat-chip-list-wrapper > mat-chip.mat-chip').eq(0).click(); //button "Adressen, mit Titel 'test'"
+    cy.get('button').contains('Entfernen').click();
+  });
 
   //saved searches
-  xit('should delete saved search', () => {});
+  it('should delete saved search', () => {
+    ResearchPage.visit();
+    ResearchPage.saveSearchProfile('testSearch', 'Test search profile to be deleted afterwards');
+    ResearchPage.openSearchOptionTab(SearchOptionTabs.SavedSearches);
+    ResearchPage.deleteSavedSearch('testSearch');
+  });
 
-  xit('should bring you back to Erweiterte Suche when saved search has been clicked', () => {});
+  it('should bring you back to Erweiterte Suche when saved search has been clicked', () => {
+    ResearchPage.visit();
+    ResearchPage.saveSearchProfile('testProfileBackToSearch', 'Test Profile for going back to Erweiterte Suche');
+    ResearchPage.openSearchOptionTab(SearchOptionTabs.SavedSearches);
+    cy.contains('mat-list-option.mat-list-item', 'testProfileBackToSearch').click();
+    //make sure you're in the 'Erweiterte Suche'-tab: this tab should be selected
+    cy.get('div.mat-tab-labels > div.mat-ripple:nth-child(1)').invoke('attr', 'aria-selected').should('eq', 'true');
+  });
 });
--- frontend/e2e/cypress/pages/research.page.ts
+++ frontend/e2e/cypress/pages/research.page.ts
@@ -37,6 +37,85 @@ export class ResearchPage {
     //without forcing cypress to click it doesn't find checkbox
     cy.get(FilterType).click({ force: true });
   }
+
+  static openSearchOptionTab(option: SearchOptionTabs) {
+    cy.get('.mat-ripple.mat-tab-label:nth-child(' + option + ')').click();
+  }
+
+  static openContextMenuSpatialReference(action: contextActionSpatial) {
+    cy.get('div.location').find('button').click();
+    cy.get('div.mat-menu-content > button:nth-child(' + action + ')').click();
+  }
+
+  static openContextMenuOfSearchResult(name: string, action: contextActionSearchResult) {
+    cy.contains('td', name).parent().find('button').click();
+    cy.get('div.mat-menu-content > button').eq(action).click();
+  }
+
+  //check first radio button, only if a list with suggestions is offered
+  static chooseLocationSuggestionByRadioButton() {
+    //wait for the list of suggestions to appear
+    cy.intercept({
+      method: 'GET',
+      path: '/search/*'
+    }).as('waitForSuggestions');
+    cy.wait('@waitForSuggestions');
+    //check if there are result suggestions in the corresponding field, if so: click the first suggestion
+    cy.get('mat-list.mat-list-base').then(resultField => {
+      if (resultField.find('mat-list-item.mat-list-item').length > 0) {
+        cy.get('mat-list.mat-list-base:nth-child(1)').click();
+      }
+    });
+  }
+
+  //when the dropdown menu will contain more than one option: TODO use parameter "type" to choose
+  static createSpatialReference(location: string, title = 'testSpatial', type = 'freeSpatialRef') {
+    cy.get('ige-facets').find('.btn-title').should('contain', 'Hinzufügen').click(); //click "Hinzufügen"
+    cy.get('ige-spatial-dialog').find('input[data-placeholder="Suchen"]').type(`${location}`); //search term
+    this.chooseLocationSuggestionByRadioButton();
+    cy.get('ige-spatial-dialog').find('input[data-placeholder="Eingeben..."]').type(`${title}`); //title
+    cy.get('ige-spatial-dialog').find('[data-cy="confirm-dialog-save"]').click(); //close editing window
+  }
+
+  static editSpatialReference(location: string, title = 'testSpatial', type = 'freeSpatialRef') {
+    ResearchPage.openContextMenuSpatialReference(contextActionSpatial.Edit);
+    cy.get('ige-spatial-dialog').find('input[data-placeholder="Suchen"]').type(`${location}`); //search term
+    this.chooseLocationSuggestionByRadioButton();
+    cy.get('ige-spatial-dialog').find('[data-cy="confirm-dialog-save"]').click(); //close editing window
+  }
+
+  static deleteSpatialReference() {
+    ResearchPage.openContextMenuSpatialReference(contextActionSpatial.Delete);
+  }
+
+  static saveSearchProfile(title: string, description: string) {
+    cy.get('ige-result-table').find('button > span:contains("Speichern")').click(); //open up save dialogue
+    cy.get('div.mat-form-field-infix >input.mat-input-element').eq(1).type(`${title}`);
+    cy.get('div.mat-form-field-infix >input.mat-input-element').eq(2).type(`${description}`);
+    cy.get('div.cdk-overlay-pane').find("button > span:contains('Speichern')").click();
+  }
+
+  static checkExistenceOfSavedSearch(title: string) {
+    //following lines: alternative containing iterating over individual list elements
+    //cy.get('mat-selection-list.mat-selection-list')
+    //.find('mat-list-option.mat-list-item')
+    //.each(function ($el, index, $list) {
+    //console.log($el.text());
+
+    cy.get('mat-selection-list.mat-selection-list')
+      .invoke('text')
+      .then(wholeText => {
+        expect(wholeText).to.include(title);
+      });
+  }
+
+  static deleteSavedSearch(title: string) {
+    cy.contains('mat-list-option.mat-list-item', title).find('button').click();
+    cy.get('div.mat-menu-content > button').click();
+    cy.get('mat-dialog-actions.mat-dialog-actions > button:contains("Löschen")').click();
+  }
+
+  static SQLField = 'textarea';
 }
 
 export enum FilterExtendedSearch {
@@ -44,4 +123,22 @@ export enum FilterExtendedSearch {
   NoFolders = '.mat-checkbox-inner-container > input[name="exceptFolders"]',
   mCloud = '.mat-checkbox-inner-container > input[name="selectDocMCloud"]',
   Test = '.mat-checkbox-inner-container > input[name="selectDocTest"]'
+}
+
+export enum SearchOptionTabs {
+  ExtendedSearch = 1,
+  SQLSearch,
+  SavedSearches
+}
+
+export enum contextActionSpatial {
+  Edit = 1,
+  Delete
+}
+
+export enum contextActionSearchResult {
+  Preview = 0,
+  Open,
+  Export,
+  Delete
 }
