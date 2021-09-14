ba44eb2b8e307c3a4f3e954b8a13afadee7eded4
Nizar Bikti <nizar.bikti@gmail.com>
2021-09-14 17:26:57 +0200

    Add receive email functionality for tests
    * chrome web security is set to false
    so that after logout the test can keep on working
    * add smtp-tester package which is a simple smtp
    server that accepts connections and receives mail
    * add tasks to cypress to get last email sent to server
    * add test

---
+++ frontend/e2e/cypress/integration/administration/email-tests.spec.ts
@@ -0,0 +1,52 @@
+import { AdminUserPage } from '../../pages/administration-user.page';
+
+
+describe('Email-tests', () => {
+  beforeEach(() => {
+    cy.kcLogin('user');
+    cy.visit('user');
+    cy.get('.page-title').contains('Nutzer');
+
+    cy.task('resetEmails')
+  });
+
+  afterEach(() => {
+    cy.kcLogout();
+  });
+
+  it('should register a user, get her password per email and log in', () => {
+    let userLogIn = 'email-test'
+    let userEmail = 'email-test@wemove.com'
+    var psw = ''
+
+    // create user
+    cy.get('button', { timeout: 5000 }).contains('Hinzufügen').click();
+    AdminUserPage.addNewUserLogin(userLogIn);
+    AdminUserPage.addNewUserFirstname(userLogIn);
+    AdminUserPage.addNewUserLastname(userLogIn);
+    AdminUserPage.addNewUserEmail(userEmail);
+    AdminUserPage.addNewUserRole('Autor');
+    cy.get('button').contains('Anlegen').parent().should('not.have.class', 'mat-button-disabled');
+    AdminUserPage.confirmAddUserDialog();
+
+    // get email and extract the password
+    cy.task('getLastEmail', userEmail)
+      .its('body')
+      .then((body) => {
+        expect(body).to.contain('Herzlich willkommen beim IGE-NG');
+
+        psw = body.substring(body.indexOf('Passwort: ') + 'Passwort: '.length, body.indexOf('(muss') - 1)
+
+        cy.kcLogout();
+        cy.get('.title', {timeout: 20000}).should('contain', 'InGrid');
+
+        cy.get('#username').type(userLogIn);
+        cy.get('#password').type(psw);
+        cy.get('#kc-login').click();
+
+        cy.get('#kc-header-wrapper').should('contain', 'Update password')
+
+      })
+
+  });
+})
