bf6fc6a4190b38bb0697f657b7c4f521ba11fecd
hani <hanidebs94@gmail.com>
2021-10-04 14:48:47 +0200

    Test: focus on a selected spatial reference and reset the view

--- frontend/e2e/cypress/integration/documents/spatial.spec.ts
+++ frontend/e2e/cypress/integration/documents/spatial.spec.ts
@@ -2,6 +2,7 @@ import { enterMcloudDocTestData } from '../../pages/enterMcloudDocTestData';
 import { DocumentPage } from '../../pages/document.page';
 import { Utils } from '../../pages/utils';
 import { Tree } from '../../pages/tree.partial';
+import Doc = Mocha.reporters.Doc;
 
 describe('Spatial References', () => {
   beforeEach(() => {
@@ -113,5 +114,25 @@ describe('Spatial References', () => {
     DocumentPage.checkSpatialEntryExistsNot('create spatial reference, wkt-1');
   });
 
-  xit('should focus on a selected spatial reference and also reset view after pressing reset button', () => {});
+  it('should focus on a selected spatial reference and also reset view after pressing reset button', () => {
+    // open document and navigate to spatial section
+    Tree.openNode(['TestDocResearch1']);
+    cy.get('[data-cy="spatialButton"]').should('not.be.visible');
+    cy.get('.navigation-header').contains('Raumbezüge').click();
+    cy.get('[data-cy="spatialButton"]', { timeout: 1000 }).should('be.visible');
+
+    // click on first Raumbezug
+    cy.get('ige-spatial-list mat-list > mat-list-item', { timeout: 1000 }).eq(0).click();
+
+    // make sure that only the first one is visible and has blue color
+    cy.get('path.leaflet-interactive:nth-of-type(1)', { timeout: 1000 }).should('be.visible');
+    cy.get('path.leaflet-interactive:nth-of-type(2)', { timeout: 1000 }).should('not.be.visible');
+    cy.get('path.leaflet-interactive:nth-of-type(3)', { timeout: 1000 }).should('not.be.visible');
+
+    DocumentPage.clickLeafletMapResetBtn();
+    //  make sure that all others are visible
+    cy.get('path.leaflet-interactive:nth-of-type(3)', { timeout: 1000 }).should('be.visible');
+    cy.get('path.leaflet-interactive:nth-of-type(2)', { timeout: 1000 }).should('be.visible');
+    cy.get('path.leaflet-interactive:nth-of-type(1)', { timeout: 1000 }).should('be.visible');
+  });
 });
