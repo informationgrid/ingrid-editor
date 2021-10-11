f9b8be97e3e24db5728d2667ca7bd412e22cb228
André Wallat <andre.wallat@wemove.com>
2021-10-11 20:35:50 +0200

    Convert UTF-8 characters in properties

--- server/src/main/java/de/ingrid/igeserver/mail/EmailServiceImpl.kt
+++ server/src/main/java/de/ingrid/igeserver/mail/EmailServiceImpl.kt
@@ -4,26 +4,16 @@ import de.ingrid.igeserver.configuration.GeneralProperties
 import de.ingrid.igeserver.configuration.MailProperties
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mail.SimpleMailMessage
-import org.springframework.mail.javamail.JavaMailSenderImpl
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Component
 import java.text.MessageFormat
-import java.util.*
 
 @Component
 class EmailServiceImpl @Autowired constructor(
-    val email: JavaMailSenderImpl,
+    val email: JavaMailSender,
     val mailProps: MailProperties,
     val appSettings: GeneralProperties
 ) {
-
-    init {
-        email.javaMailProperties =
-            Properties().apply {
-                setProperty("mail.mime.charset", "utf-8");
-                setProperty("mail.smtp.allow8bitmime", "true");
-                setProperty("mail.smtps.allow8bitmime", "true");
-            }
-    }
 
     fun sendWelcomeEmail(to: String, firstName: String, lastName: String) {
         val message = SimpleMailMessage().apply {
@@ -44,6 +34,5 @@ class EmailServiceImpl @Autowired constructor(
         }
         email.send(message)
     }
-
 
 }
