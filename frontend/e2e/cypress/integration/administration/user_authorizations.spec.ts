9358d4380663023861ce4fae815dfc10fafdfa3a
hani <hanidebs94@gmail.com>
2021-10-12 15:04:20 +0200

    Test: try to move/eidt/copy  folders and documents and create new file for it.

--- frontend/e2e/cypress/integration/administration/user_authorizations.spec.ts
+++ frontend/e2e/cypress/integration/administration/user_authorizations.spec.ts
@@ -8,17 +8,11 @@ import { UserAuthorizationPage } from '../../pages/user_authorizations.page';
 import { Tree } from '../../pages/tree.partial';
 import { AdminGroupPage, headerKeys } from '../../pages/administration-group.page';
 import { CopyCutUtils } from '../../pages/copy-cut-utils';
-import Doc = Mocha.reporters.Doc;
 
 // meta data administrator without groups
 describe('Meta data administrator without groups', () => {
   beforeEach(() => {
-    try {
-      cy.kcLogout();
-      cy.kcLogin('meta');
-    } catch (error) {
-      console.log(error);
-    }
+    cy.kcLogin('meta');
   });
 
   afterEach(() => {
@@ -175,88 +169,6 @@ describe('Meta data administrator with a group', () => {
     AdminGroupPage.selectGroup('gruppe_mit_ortsrechten');
     cy.get('.user-title').contains('gruppe_mit_ortsrechten');
     UserAuthorizationPage.changeAccessRightFromReadToWrite('test_z, test_z', 'Adressen');
-    AdminGroupPage.toolbarSaveGroup();
-  });
-
-  it('meta data admin with groups should not be able to edit/move/delete a data document of his assigned groups if access is read-only (#2778)', () => {
-    // set access to read-only
-    cy.visit('user');
-    AdminUserPage.goToTabmenu(UserAndRights.Group);
-    AdminGroupPage.selectGroup('test_gruppe_1');
-    cy.get('.user-title').contains('test_gruppe_1');
-    UserAuthorizationPage.changeAccessRightFromWriteToRead('Ordner_Ebene_2C', 'Daten');
-    AdminGroupPage.toolbarSaveGroup();
-
-    // try to edit
-    DocumentPage.visit();
-    Tree.openNode(['Ordner_Ebene_2C', 'Ordner_Ebene_3C', 'Datum_Ebene_4_5']);
-    // if editing is forbidden, the form fields are disabled
-    cy.get('mat-form-field.mat-form-field-disabled');
-
-    // try to move
-    cy.get('[data-cy=toolbar_COPY]').click();
-    cy.get('[data-cy="copyMenu_CUT"]').should('be.disabled');
-
-    // try to delete
-    cy.get(DocumentPage.Toolbar['Delete']).should('be.disabled');
-
-    // set access right back to 'write'
-    cy.visit('user');
-    AdminUserPage.goToTabmenu(UserAndRights.Group);
-    AdminGroupPage.selectGroup('test_gruppe_1');
-    cy.get('.user-title').contains('test_gruppe_1');
-    UserAuthorizationPage.changeAccessRightFromReadToWrite('Ordner_Ebene_2C', 'Daten');
-    AdminGroupPage.toolbarSaveGroup();
-  });
-
-  it('meta data admin with groups should not be able to move a data document to a read-only folder', () => {
-    const readOnlyFolder = 'Ordner_Ebene_2C';
-    const folderToMove = 'Ordner_Ebene_3A';
-    const documentToMove = 'Datum_Ebene_3_3';
-
-    // set access to read-only
-    cy.visit('user');
-    AdminUserPage.goToTabmenu(UserAndRights.Group);
-    AdminGroupPage.selectGroup('test_gruppe_1');
-    cy.get('.user-title').contains('test_gruppe_1');
-    UserAuthorizationPage.changeAccessRightFromWriteToRead(readOnlyFolder, 'Daten');
-    AdminGroupPage.toolbarSaveGroup();
-
-    // try to move a folder to the read-only folder
-    DocumentPage.visit();
-    Tree.openNode(['Ordner_Ebene_2A', folderToMove]);
-    cy.get('[data-cy=toolbar_COPY]').click();
-    cy.get('[data-cy="copyMenu_COPYTREE"]').click();
-    cy.contains('mat-dialog-content', readOnlyFolder).should('not.exist');
-    cy.get('[data-cy="dlg-close"]').click();
-
-    // try to move this folder via drag and drop to read-only folder
-    CopyCutUtils.simpleDragdropWithoutAutoExpand(folderToMove, readOnlyFolder);
-    AdminUserPage.attemptIllegitimateMove();
-    // expect error
-    cy.get('error-dialog').contains('keine Berechtigung');
-    UserAuthorizationPage.closeErrorBox();
-
-    // try to move a document to the read-only folder
-    Tree.openNode(['Ordner_Ebene_2A', documentToMove]);
-    cy.get('[data-cy=toolbar_COPY]').click();
-    cy.get('[data-cy="copyMenu_COPY"]').click();
-    cy.contains('mat-dialog-content', readOnlyFolder).should('not.exist');
-    cy.get('[data-cy="dlg-close"]').click();
-
-    // try to move this document via drag and drop to read-only folder
-    CopyCutUtils.simpleDragdropWithoutAutoExpand(documentToMove, readOnlyFolder);
-    AdminUserPage.attemptIllegitimateMove();
-    // expect error
-    cy.get('error-dialog').contains('keine Berechtigung');
-    UserAuthorizationPage.closeErrorBox();
-
-    // set access right back to 'write'
-    cy.visit('user');
-    AdminUserPage.goToTabmenu(UserAndRights.Group);
-    AdminGroupPage.selectGroup('test_gruppe_1');
-    cy.get('.user-title').contains('test_gruppe_1');
-    UserAuthorizationPage.changeAccessRightFromReadToWrite(readOnlyFolder, 'Daten');
     AdminGroupPage.toolbarSaveGroup();
   });
 
