ff8d9e9d5fb34226710b63890415cbb62ea00f6f
André Wallat <andre.wallat@wemove.com>
2021-10-19 11:19:25 +0200

    Test: Fix Email Test

--- frontend/e2e/cypress/integration/administration/email-tests.spec.ts
+++ frontend/e2e/cypress/integration/administration/email-tests.spec.ts
@@ -1,19 +1,18 @@
 import { AdminUserPage } from '../../pages/administration-user.page';
 
-
 describe('Email-tests', () => {
   beforeEach(() => {
     cy.kcLogin('user');
     cy.visit('user');
     cy.get('.page-title').contains('Nutzer');
 
-    cy.task('resetEmails')
+    cy.task('resetEmails');
   });
-  
+
   it('should register a user, get her password per email and log in', () => {
-    let userLogIn = 'email-test'
-    let userEmail = 'email-test@wemove.com'
-    var psw = ''
+    let userLogIn = 'email-test';
+    let userEmail = 'email-test@wemove.com';
+    var psw = '';
 
     // create user
     cy.get('button', { timeout: 5000 }).contains('Hinzufügen').click();
@@ -28,21 +27,19 @@ describe('Email-tests', () => {
     // get email and extract the password
     cy.task('getLastEmail', userEmail)
       .its('body')
-      .then((body) => {
-        expect(body).to.contain('Herzlich willkommen beim IGE-NG');
+      .then(body => {
+        expect(body).to.contain('Herzlich Willkommen beim IGE-NG');
 
-        psw = body.substring(body.indexOf('Passwort: ') + 'Passwort: '.length, body.indexOf('(muss') - 1)
+        psw = body.substring(body.indexOf('Passwort: ') + 'Passwort: '.length, body.indexOf('(muss') - 1);
 
         cy.kcLogout();
-        cy.get('.title', {timeout: 20000}).should('contain', 'InGrid');
+        cy.get('.title', { timeout: 20000 }).should('contain', 'InGrid');
 
         cy.get('#username').type(userLogIn);
         cy.get('#password').type(psw);
         cy.get('#kc-login').click();
 
-        cy.get('#kc-header-wrapper').should('contain', 'Update password')
-
-      })
-
+        cy.get('#kc-header-wrapper').should('contain', 'Update password');
+      });
   });
-})
+});
