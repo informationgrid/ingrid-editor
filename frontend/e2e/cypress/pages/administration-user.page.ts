3825b227608a71eb10e9f23bed8af9e88bec9d4b
hani <hanidebs94@gmail.com>
2021-11-09 12:01:29 +0100

    Test: adjust failing test add intercept instead of wait fixed time

--- frontend/e2e/cypress/pages/administration-user.page.ts
+++ frontend/e2e/cypress/pages/administration-user.page.ts
@@ -119,8 +119,9 @@ export class AdminUserPage extends BasePage {
     // when parameter manager is there, the action is not expected to fail
     cy.contains('mat-dialog-container', 'Der Benutzer ist aktuell für folgende Nutzer verantwortlich');
     cy.contains('button', 'Verantwortlichen auswählen').click();
+    cy.intercept('GET', '/api/users/admins').as('getAdmins');
     cy.get('mat-dialog-container .mat-select-arrow').trigger('mouseover').click({ force: true });
-    cy.wait(5000);
+    cy.wait('@getAdmins');
     cy.contains('[role="listbox"] mat-option', manager, { timeout: 10000 }).click();
     cy.intercept('POST', '/api/users/' + '*' + '/manager' + '*').as('waitForSelection');
     cy.contains('mat-dialog-container button', 'Zuweisen').click();
