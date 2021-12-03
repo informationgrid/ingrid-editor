04c7932242d1b28cc72adfcf9bacb320923781e5
Lisa Karolina Raschke <lisa.karolina.raschke@wemove.com>
2021-12-03 14:11:17 +0100

    Test: remove intercept that continuously fails to capture the right request

--- frontend/e2e/cypress/integration/documents/spatial.spec.ts
+++ frontend/e2e/cypress/integration/documents/spatial.spec.ts
@@ -50,9 +50,7 @@ describe('Spatial References', () => {
     const docNameBbox = 'spatialwkt-' + Utils.randomString();
 
     // create new document
-    cy.intercept('GET', /api\/codelist\/[0-9]+/).as('createRequest');
     DocumentPage.CreateSpatialWKTWithAPI(docNameBbox, false);
-    cy.wait('@createRequest', { timeout: 10000 });
     // reload the page so that the new document is visible
     DocumentPage.reloadPage();
     Tree.openNode(['api-' + docNameBbox]);
