f5a303a7be0d4e400d22cfed4d438eb39e97829a
Lisa Karolina Raschke <lisa.karolina.raschke@wemove.com>
2022-01-13 18:54:03 +0100

    Test: add import tests

---
+++ frontend/e2e/cypress/integration/import_export/import.spec.ts
@@ -0,0 +1,116 @@
+import { ImportOptions, ImportPage } from '../../pages/import.page';
+import { Tree } from '../../pages/tree.partial';
+import { DocumentPage } from '../../pages/document.page';
+import { AddressPage } from '../../pages/address.page';
+import { enterMcloudDocTestData } from '../../pages/enterMcloudDocTestData';
+
+describe('Import Tests', () => {
+  beforeEach(() => {
+    cy.kcLogout();
+    cy.kcLogin('user').as('tokens');
+    ImportPage.visit();
+  });
+
+  it('should import json document (new document, new address) with option "Anlage unter gewähltem Importknoten"', () => {
+    const fileTitle = 'importtest_2.json';
+
+    ImportPage.addFile(fileTitle);
+    ImportPage.continue();
+    ImportPage.chooseImportOption(ImportOptions.CreateUnderTarget);
+    ImportPage.continue();
+    Tree.openNode(['Testdokumente'], false, false, true);
+    Tree.openNode(['Neue Testadressen'], false, false, true);
+    ImportPage.closeDialogAndImport();
+    ImportPage.jumpToDocument('document_import_32');
+    Tree.checkPath(['Daten', 'Testdokumente']);
+    cy.get('ige-address-card').should('exist');
+    DocumentPage.jumpFromDocumentToAddress('Transsilvanien, Adresse');
+    Tree.checkTitleOfSelectedNode('Transsilvanien, Adresse');
+    Tree.checkPath(['Adressen', 'Neue Testadressen']);
+  });
+
+  it('should import json document (new document, existing address) with option "Anlage unter gewähltem Importknoten"', () => {
+    const fileTitle = 'importtest_1.json';
+
+    ImportPage.addFile(fileTitle);
+    ImportPage.continue();
+    ImportPage.chooseImportOption(ImportOptions.CreateUnderTarget);
+    ImportPage.continue();
+    Tree.openNode(['Testdokumente'], false, false, true);
+    Tree.openNode(['Testadressen'], false, false, true);
+    ImportPage.closeDialogAndImport();
+    ImportPage.jumpToDocument('document_import_12');
+    Tree.checkPath(['Daten', 'Testdokumente']);
+    cy.get('ige-address-card').should('exist');
+    DocumentPage.jumpFromDocumentToAddress('Aquitanien, Adresse', 73);
+    Tree.checkTitleOfSelectedNode('Aquitanien, Adresse');
+    Tree.checkPath(['Adressen', 'Neue Testadressen', 'Ordner 2. Ebene']);
+  });
+
+  it('should import json document (new document, new address) with option "identische Metadaten überschreiben"', () => {
+    const fileTitle = 'importtest_3.json';
+
+    ImportPage.addFile(fileTitle);
+    ImportPage.continue();
+    ImportPage.chooseImportOption(ImportOptions.OverwriteMetadata);
+    ImportPage.continue();
+    Tree.openNode(['Testdokumente'], false, false, true);
+    Tree.openNode(['Testadressen'], false, false, true);
+    ImportPage.closeDialogAndImport();
+    ImportPage.jumpToDocument('document_import_43');
+    Tree.checkPath(['Daten', 'Testdokumente']);
+    cy.get('ige-address-card').should('exist');
+    DocumentPage.jumpFromDocumentToAddress('Sibirien, Adresse');
+    Tree.checkTitleOfSelectedNode('Sibirien, Adresse');
+    Tree.checkPath(['Adressen', 'Testadressen']);
+  });
+
+  xit('should import json document (existing document, new address) with option "identische Metadaten überschreiben"', () => {
+    const fileTitle = 'importtest_4.json';
+
+    ImportPage.addFile(fileTitle);
+    ImportPage.continue();
+    ImportPage.chooseImportOption(ImportOptions.OverwriteMetadata);
+    ImportPage.continue();
+    Tree.openNode(['Neue Testdokumente', 'Ordner_Ebene_2A', 'Ordner_Ebene_3B'], false, false, true);
+    Tree.openNode(['Testadressen'], false, false, true);
+    ImportPage.closeDialogAndImport();
+    ImportPage.jumpToDocument('Datum_Ebene_4_4');
+    Tree.checkPath(['Daten', 'Neue Testdokumente', 'Ordner_Ebene_2A', 'Ordner_Ebene_3B']);
+    cy.get('ige-address-card').should('exist');
+    DocumentPage.jumpFromDocumentToAddress('Schlesien, Adresse');
+    Tree.checkTitleOfSelectedNode('Schlesien, Adresse');
+    Tree.checkPath(['Adressen', 'Testadressen']);
+  });
+
+  xit('should import json document (existing document, existing address) with option "identische Metadaten überschreiben"', () => {
+    const fileTitle = 'importtest_5.json';
+
+    // add address to document
+    DocumentPage.visit();
+    Tree.openNode(['Testdokumente', 'Ordner 2. Ebene', 'Tiefes Dokument']);
+    enterMcloudDocTestData.CreateDialog.setAddress('Franken, Adresse');
+    AddressPage.saveDocument();
+    // import file
+    ImportPage.visit();
+    ImportPage.addFile(fileTitle);
+    ImportPage.continue();
+    ImportPage.chooseImportOption(ImportOptions.OverwriteMetadata);
+    ImportPage.continue();
+    Tree.openNode(['Testdokumente', 'Ordner 2. Ebene'], false, false, true);
+    Tree.openNode(['Neue Testadressen'], false, false, true);
+    ImportPage.closeDialogAndImport();
+    ImportPage.jumpToDocument('Tiefes Dokument');
+    DocumentPage.jumpFromDocumentToAddress('Franken, Adresse', 82);
+    Tree.checkTitleOfSelectedNode('Franken, Adresse');
+    Tree.checkPath(['Adressen', 'Neue Testadressen']);
+  });
+
+  it('should give hint when trying to import not-importable file', () => {
+    ImportPage.tryAddFileInWrongFormat('Test.pdf');
+    cy.get('.mat-line.mat-error').should('contain', 'No importer found');
+    // make sure file is not accepted when retrying
+    cy.contains('button', 'Erneut versuchen').click();
+    cy.contains('button', 'Weiter').should('be.disabled');
+  });
+});
---
+++ frontend/e2e/cypress/pages/import.page.ts
@@ -0,0 +1,44 @@
+import { Tree } from '../pages/tree.partial';
+
+export class ImportPage {
+  static visit() {
+    cy.intercept('GET', /api\/import\?profile=mcloud/).as('getImportPage');
+    cy.visit('/importExport/import');
+    cy.wait('@getImportPage', { timeout: 10000 });
+  }
+
+  static addFile(filePath: string) {
+    cy.intercept('POST', /api\/import\/analyze/).as('upload');
+    cy.get('[type="file"]').attachFile(filePath);
+    cy.wait('@upload', { timeout: 10000 });
+    cy.get('.success-icon');
+  }
+
+  static tryAddFileInWrongFormat(filePath: string) {
+    cy.intercept('POST', /api\/import\/analyze/).as('tryUpload');
+    cy.get('[type="file"]').attachFile(filePath);
+    cy.wait('@tryUpload', { timeout: 10000 }).its('response.statusCode').should('equal', 500);
+  }
+
+  static continue() {
+    cy.contains('button', 'Weiter').click({ force: true });
+  }
+
+  static chooseImportOption(option: ImportOptions) {
+    cy.get(' mat-radio-button [value="' + option + '"]').click({ force: true });
+  }
+
+  static closeDialogAndImport() {
+    cy.contains('button', 'Importieren').click();
+  }
+
+  static jumpToDocument(docName: string) {
+    cy.contains('button', 'Öffnen').click();
+    cy.get('.title').should('contain', docName);
+  }
+}
+
+export enum ImportOptions {
+  OverwriteMetadata = 'overwrite_identical',
+  CreateUnderTarget = 'create_under_target'
+}
