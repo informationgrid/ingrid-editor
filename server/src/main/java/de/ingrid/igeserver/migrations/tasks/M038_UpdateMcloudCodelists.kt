875bd878f5b40d22fde5c108f573e103ad722f0d
André Wallat <andre.wallat@wemove.com>
2022-01-22 13:24:58 +0100

    Add migration strategy inside a transaction
    This is needed since database is being manipulated

--- server/src/main/java/de/ingrid/igeserver/migrations/tasks/M038_UpdateMcloudCodelists.kt
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M038_UpdateMcloudCodelists.kt
@@ -1,13 +1,14 @@
 package de.ingrid.igeserver.migrations.tasks
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import de.ingrid.igeserver.migrations.MigrationBase
+import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
 import de.ingrid.igeserver.persistence.postgresql.jpa.model.ige.Catalog
 import de.ingrid.igeserver.repository.CatalogRepository
 import de.ingrid.igeserver.services.CatalogService
 import org.apache.logging.log4j.kotlin.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
 
 @Service
 class M038_UpdateMcloudCodelists : MigrationBase("0.38") {
@@ -15,27 +16,30 @@ class M038_UpdateMcloudCodelists : MigrationBase("0.38") {
     private var log = logger()
 
     @Autowired
-    @JsonIgnore
     lateinit var catalogRepo: CatalogRepository
 
     @Autowired
-    @JsonIgnore
     lateinit var catalogService: CatalogService
+
+    @Autowired
+    private lateinit var transactionManager: PlatformTransactionManager
 
 
     override fun exec() {
         log.info("Update mCLOUD Codelists!")
 
-        try {
-            val mCloudProfile = catalogService.getCatalogProfile("mcloud")
-            catalogRepo.findAll()
-                .filter { catalog: Catalog -> catalog.type == mCloudProfile.identifier }
-                .forEach { catalog: Catalog ->
-                    log.info("Update Catalog: "+catalog.name)
-                    catalogService.initializeCodelists(catalog.identifier, catalog.type)
-                }
-        } catch (e: NullPointerException) {
-            log.debug("Cannot update mcloud codelists, since mcloud profile is not activated")
+        ClosableTransaction(transactionManager).use {
+            try {
+                val mCloudProfile = catalogService.getCatalogProfile("mcloud")
+                catalogRepo.findAll()
+                    .filter { catalog: Catalog -> catalog.type == mCloudProfile.identifier }
+                    .forEach { catalog: Catalog ->
+                        log.info("Update Catalog: " + catalog.name)
+                        catalogService.initializeCodelists(catalog.identifier, catalog.type)
+                    }
+            } catch (e: NullPointerException) {
+                log.debug("Cannot update mcloud codelists, since mcloud profile is not activated")
+            }
         }
     }
 
