d6197e909488b4c1197f4784f9bf9c9aae370e43
hani <hanidebs94@gmail.com>
2022-01-26 12:06:51 +0100

    Revert "Test: change browser resolution"
    This reverts commit 543e809c8c665b6efdcf89d2af9ca2d087d67a4e.

--- frontend/e2e/cypress/plugins/index.js
+++ frontend/e2e/cypress/plugins/index.js
@@ -7,7 +7,7 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-const ms = require('smtp-tester');
+const ms = require('smtp-tester')
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
@@ -17,47 +17,42 @@ module.exports = (on, config) => {
   // `config` is the resolved Cypress config
 
   // email configuration
-  const port = 25;
-  const mailServer = ms.init(port);
+  const port = 25
+  const mailServer = ms.init(port)
 
-  console.log('Mail server started at port %d', port);
+  console.log('Mail server started at port %d', port)
 
-  let lastEmail = {};
+  let lastEmail = {}
 
   // process all emails
   mailServer.bind((addr, id, email) => {
-    console.log('--- email to %s ---', email.headers.to);
-    console.log(email.body);
-    console.log('--- end ---');
+    console.log('--- email to %s ---', email.headers.to)
+    console.log(email.body)
+    console.log('--- end ---')
 
     // store the email by the receiver email
     lastEmail[email.headers.to] = {
       body: email.body,
-      html: email.html
-    };
-  });
+      html: email.html,
+    }
+  })
 
   on('task', {
     // clean email inbox
     resetEmails(email) {
       if (email) {
-        delete lastEmail[email];
+        delete lastEmail[email]
       } else {
-        lastEmail = {};
+        lastEmail = {}
       }
-      return null;
+      return null
     },
 
     getLastEmail(userEmail) {
       // cy.task cannot return undefined
       // thus we return null as a fallback
-      return lastEmail[userEmail] || null;
-    }
-  });
-  on('before:browser:launch', (browser = {}, args) => {
-    if (browser.name === 'chrome' && browser.isHeadless) {
-      args.push('--window-size=900,660');
-      return args;
-    }
-  });
+      return lastEmail[userEmail] || null
+    },
+  })
+
 };
