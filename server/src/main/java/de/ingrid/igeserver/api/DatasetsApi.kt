0f8d6a4bad64f184af0c302c77017f54d82990b8
André Wallat <andre.wallat@wemove.com>
2022-02-02 14:38:00 +0100

    Revert "Add debug for duration of getChildren function"
    This reverts commit d9a529e71f585c790b4fb478b462f7c08c657a20.

--- server/src/main/java/de/ingrid/igeserver/api/DatasetsApi.kt
+++ server/src/main/java/de/ingrid/igeserver/api/DatasetsApi.kt
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.security.Principal
 import javax.validation.Valid
-import kotlin.time.ExperimentalTime
 
 @Tag(name = "Datasets", description = "the datasets API")
 interface DatasetsApi {
@@ -64,7 +63,6 @@ interface DatasetsApi {
             principal: Principal,
             @Parameter(description = "The ID of the dataset.", required = true) @PathVariable("id") ids: Array<String>): ResponseEntity<Unit>
 
-    @ExperimentalTime
     @Operation(description = "Get child datasets of a given parent document/folder")
     @ApiResponses(value = [ApiResponse(responseCode = "200", description = "Child Datasets found")])
     @RequestMapping(value = ["/tree/children"], produces = [MediaType.APPLICATION_JSON_VALUE], method = [RequestMethod.GET])
