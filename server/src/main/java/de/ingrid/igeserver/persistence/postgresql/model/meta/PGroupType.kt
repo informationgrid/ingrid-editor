4b722f44672874b77cd5f914867c9f67731d7548
André Wallat <andre.wallat@wemove.com>
2022-02-07 21:56:01 +0100

    Ignore unused permission types

--- server/src/main/java/de/ingrid/igeserver/persistence/postgresql/model/meta/PGroupType.kt
+++ server/src/main/java/de/ingrid/igeserver/persistence/postgresql/model/meta/PGroupType.kt
@@ -1,12 +1,14 @@
 package de.ingrid.igeserver.persistence.postgresql.model.meta
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.node.ArrayNode
 import org.springframework.stereotype.Component
 
 @Component
 data class PermissionsData(
+    @JsonIgnoreProperties
     val pages: JsonNode? = null,
+    @JsonIgnoreProperties
     val actions: JsonNode? = null,
     var documents: List<JsonNode>? = null,
     var addresses: List<JsonNode>? = null
