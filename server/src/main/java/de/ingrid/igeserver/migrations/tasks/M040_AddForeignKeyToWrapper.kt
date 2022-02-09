983114b8cf3d4b36fae23d1b1f5d30aae6ffce31
André Wallat <andre.wallat@wemove.com>
2022-02-09 11:11:55 +0100

    Add foreign keys to document_wrapper table
    This makes sure that only one document with uuid and catalogId can exist

---
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M040_AddForeignKeyToWrapper.kt
@@ -0,0 +1,31 @@
+package de.ingrid.igeserver.migrations.tasks
+
+import de.ingrid.igeserver.migrations.MigrationBase
+import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
+import javax.persistence.EntityManager
+
+@Service
+class M040_AddForeignKeyToWrapper : MigrationBase("0.40") {
+
+    @Autowired
+    lateinit var entityManager: EntityManager
+
+    @Autowired
+    private lateinit var transactionManager: PlatformTransactionManager
+
+    private val sql = """
+        alter table document_wrapper
+            add constraint document_wrapper_fk_uuid_catalog
+            unique (uuid, catalog_id);
+    """.trimIndent()
+
+    override fun exec() {
+        ClosableTransaction(transactionManager).use {
+            entityManager.createNativeQuery(sql).executeUpdate()
+        }
+    }
+
+}
