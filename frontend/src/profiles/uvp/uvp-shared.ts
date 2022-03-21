ea2efa4c133928b1b67462128d7ec84abee2e847
André Wallat <andre.wallat@wemove.com>
2022-03-21 10:30:46 +0100

    Fix UVP URLs for uploaded documents (#3602)

--- frontend/src/profiles/uvp/uvp-shared.ts
+++ frontend/src/profiles/uvp/uvp-shared.ts
@@ -44,7 +44,7 @@ export class UvpShared extends BaseDoctype {
           } else {
             return `<a href="${
               this.configService.getConfiguration().backendUrl
-            }upload/${form.parent.parent.get("_uuid").value}/${
+            }upload/${form.get("_uuid").value}/${
               link.uri
             }" class="no-text-transform">${link.uri}</a>`;
           }
