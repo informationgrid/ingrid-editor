3dcbeb3c6892a1d3fc9561eba851d3d586226104
Jan Buecher <51136390+John-Books@users.noreply.github.com>
2022-03-24 16:48:43 +0100

    Handle WKT during indexing

--- server/src/main/java/de/ingrid/igeserver/profiles/mcloud/exporter/model/SpatialModel.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/mcloud/exporter/model/SpatialModel.kt
@@ -19,26 +19,51 @@ data class SpatialModel(val type: String?, val title: String?, val value: Boundi
             }
         }
 
+    val geoType: String?
+        get() {
+            return when (type) {
+                "free" -> "polygon"
+                "wkt" -> getWktType()
+                else -> null
+            }
+        }
+
     private fun getFreeCoordinates(): String? {
         if (value == null) {
             return null
         }
 
-        return "[[${value.lon1}, ${value.lat1}], [${value.lon2}, ${value.lat1}], [${value.lon2}, ${value.lat2}], [${value.lon1}, ${value.lat2}], [${value.lon1}, ${value.lat1}]]"
+        return "[[[${value.lon1}, ${value.lat1}], [${value.lon2}, ${value.lat1}], [${value.lon2}, ${value.lat2}], [${value.lon1}, ${value.lat2}], [${value.lon1}, ${value.lat1}]]]"
 
     }
 
     private fun getWktCoordinates(): String? {
         if (this.wkt != null) {
             try{
-                var coordsPos = wkt.indexOf("(");
-                var coords = wkt.substring(coordsPos).trim();
+                var coordsPos = wkt.indexOf("(")
+                var coords = wkt.substring(coordsPos).trim()
+                coords = coords.replace("\n", " ")
                 coords = coords.replace("\\(".toRegex(), "[").replace("\\)".toRegex(), "]")
                 coords = coords.replace("\\[(\\s*[-0-9][^\\]]*\\,[^\\]]*[0-9]\\s*)\\]".toRegex(), "[[$1]]")
                 coords = coords.replace("([0-9])\\s*\\,\\s*([-0-9])".toRegex(), "$1], [$2")
                 coords = coords.replace("([0-9])\\s+([-0-9])".toRegex(), "$1, $2")
 
-                return coords;
+                return coords
+            } catch (ex:Exception){
+                log.error(ex)
+            }
+        }
+
+        return null
+    }
+
+    private fun getWktType(): String? {
+        if (this.wkt != null) {
+            try{
+                var coordsPos = wkt.indexOf("(")
+                var wktType = wkt.substring(0, coordsPos).trim().lowercase()
+
+                return wktType
             } catch (ex:Exception){
                 log.error(ex)
             }
