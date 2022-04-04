c06cd2c1e7f14272fe07de988f7e384c6ba3543b
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2022-04-04 17:22:47 +0200

    Fix authentification vor settings component

--- frontend/src/app/+settings/settings.routing.ts
+++ frontend/src/app/+settings/settings.routing.ts
@@ -3,11 +3,14 @@ import { SettingsComponent } from "./settings.component";
 import { GeneralSettingsComponent } from "./general-settings/general-settings.component";
 import { CodelistsComponent } from "./codelists/codelists.component";
 import { CatalogManagementComponent } from "./catalog-management/catalog-management.component";
+import { AuthGuard } from "../security/auth.guard";
 
 export const routing = RouterModule.forChild([
   {
     path: "",
     component: SettingsComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ["admin"], permission: "manage_catalog" },
     children: [
       {
         path: "",
