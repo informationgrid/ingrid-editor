96e66a83b09142e129c6c1c8b34563f80eabe925
André Wallat <andre.wallat@wemove.com>
2022-04-25 21:53:14 +0200

    Adapt files with context path

--- server/src/main/jib/run.sh
+++ server/src/main/jib/run.sh
@@ -3,5 +3,9 @@ if [[ -n "${WAIT_FOR_PARAM}" ]]; then
   /wait-for-it.sh ${WAIT_FOR_PARAM} -t 120
 fi
 
+echo "Adapt index.html to match context path"
+sed -i -r "s@href=\"/\"@href=\"$CONTEXT_PATH/\"@" /app/resources/static/index.html
+sed -i -r "s@contextPath\": \"/\"@contextPath\": \"$CONTEXT_PATH/\"@" /app/resources/static/assets/config.json
+
 echo "Run original entrypoint command"
 java -cp $( cat /app/jib-classpath-file ) $( cat /app/jib-main-class-file )
