1f403e711dc29c4076e3cc22d300af7e9ac1ce0a
André Wallat <andre.wallat@wemove.com>
2022-04-28 12:03:57 +0200

    Fix run script

--- server/src/main/jib/run.sh
+++ server/src/main/jib/run.sh
@@ -8,7 +8,7 @@ sed -i -r "s@href=\"/\"@href=\"$CONTEXT_PATH/\"@" /app/resources/static/index.ht
 sed -i -r "s@contextPath\": \"/\"@contextPath\": \"$CONTEXT_PATH/\"@" /app/resources/static/assets/config.json
 
 if [[ -n "${BROKER_URL}" ]]; then
-  sed -i -r "s@brokerUrl\":.*@brokerUrl\": \"$BROKER_URL/\",@" /app/resources/static/assets/config.json
+  sed -i -r "s@brokerUrl\":.*@brokerUrl\": \"$BROKER_URL\",@" /app/resources/static/assets/config.json
 fi
 
 echo "Run original entrypoint command"
