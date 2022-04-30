5611f3f79ca2c0a15f64c7b000f59ebfd3920eee
André Wallat <andre.wallat@wemove.com>
2022-04-30 21:34:43 +0200

    Show menu for privileged users to replace address

--- frontend/src/app/+form/form-info/header-title-row/header-title-row.component.ts
+++ frontend/src/app/+form/form-info/header-title-row/header-title-row.component.ts
@@ -12,6 +12,7 @@ import { FormGroup } from "@angular/forms";
 import { ProfileService } from "../../../services/profile.service";
 import { IgeDocument } from "../../../models/ige-document";
 import { DocumentUtils } from "../../../services/document.utils";
+import { ConfigService } from "../../../services/config/config.service";
 
 @Component({
   selector: "ige-header-title-row",
@@ -32,6 +33,7 @@ export class HeaderTitleRowComponent implements OnInit {
 
   @Input() sections: string[];
   @Input() disableEdit: boolean;
+  @Input() address: boolean;
 
   @ViewChild("titleInput") titleInput: ElementRef;
   @ViewChild("cfcAutosize") contentFCAutosize: CdkTextareaAutosize;
@@ -42,13 +44,19 @@ export class HeaderTitleRowComponent implements OnInit {
   icon: string;
   showTitleInput = false;
   showMore = false;
+  showMoreActions = false;
 
   constructor(
     private cdRef: ChangeDetectorRef,
-    private profileService: ProfileService
+    private profileService: ProfileService,
+    private configService: ConfigService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const role = this.configService.$userInfo.value.role;
+    const isPrivileged = role === "ige-super-admin" || role === "cat-admin";
+    this.showMoreActions = this.address && isPrivileged;
+  }
 
   editTitle() {
     this.showTitleInput = !this.showTitleInput;
