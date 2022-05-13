e47b8d251f6c9955485e44c480b5a87a1c5cf048
André Wallat <andre.wallat@wemove.com>
2022-05-13 14:13:29 +0200

    Fix wrong null conversion

--- server/src/main/java/de/ingrid/igeserver/profiles/uvp/exporter/model/UVPModel.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/uvp/exporter/model/UVPModel.kt
@@ -151,7 +151,9 @@ data class UVPModel(
     
     fun hasPoBox(): Boolean = !pointOfContact?.address?.poBox.isNullOrEmpty()
 
-    fun getUvpAddressParents(): List<AddressShort> = getUvpAddressParents(pointOfContact!!.parent)
+    fun getUvpAddressParents(): List<AddressShort> =
+        if (pointOfContact!!.parent == null) emptyList() else getUvpAddressParents(pointOfContact!!.parent!!)
+
     fun getUvpAddressParentsIncludingCurrent(): List<AddressShort> = getUvpAddressParents(pointOfContact!!.id)
     
     private fun getUvpAddressParents(parent: Int): List<AddressShort> {
