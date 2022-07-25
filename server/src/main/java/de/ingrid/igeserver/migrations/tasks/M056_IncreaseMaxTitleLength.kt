828393e702f05e508e8570321bb2641514c12444
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2022-07-25 17:33:12 +0200

    Add patch to increase maximum doc title length
    (cherry picked from commit a2a32ed87aec3e4ce6e49d1d13aca25f5bf6c41d)

---
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M056_IncreaseMaxTitleLength.kt
@@ -0,0 +1,30 @@
+package de.ingrid.igeserver.migrations.tasks
+
+import de.ingrid.igeserver.migrations.MigrationBase
+import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
+import javax.persistence.EntityManager
+
+@Service
+class M056_IncreaseMaxTitleLength : MigrationBase("0.56") {
+
+    @Autowired
+    lateinit var entityManager: EntityManager
+
+    @Autowired
+    private lateinit var transactionManager: PlatformTransactionManager
+
+    private val sqlUpdate = """
+    alter table document
+        alter column title type varchar(4096);
+    """.trimIndent()
+
+    override fun exec() {
+        ClosableTransaction(transactionManager).use {
+            entityManager.createNativeQuery(sqlUpdate).executeUpdate()
+        }
+    }
+
+}
