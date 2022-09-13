ce9c9396534de191b506ca243b174e869fb2bc4d
André Wallat <andre.wallat@wemove.com>
2022-09-13 14:23:36 +0200

    Add indexing of addresses for UVP (#4190)
    This is needed to show the provider on the portal page.

---
+++ server/src/main/java/de/ingrid/igeserver/profiles/uvp/exporter/IndexExporterAddress.kt
@@ -0,0 +1,32 @@
+package de.ingrid.igeserver.profiles.uvp.exporter
+
+import de.ingrid.igeserver.exports.ExportTypeInfo
+import de.ingrid.igeserver.exports.IgeExporter
+import de.ingrid.igeserver.persistence.postgresql.jpa.model.ige.Document
+import de.ingrid.igeserver.services.DocumentCategory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Service
+
+@Service
+@Profile("uvp")
+class IndexExporterAddress @Autowired constructor() : IgeExporter {
+
+    override val typeInfo = ExportTypeInfo(
+        DocumentCategory.ADDRESS,
+        "indexUvpIDF",
+        "UVP IDF Address (Elasticsearch)",
+        "Export von UVP Adressen ins IDF Format für die Anzeige im Portal ins Elasticsearch-Format. Für UVP allerdings leer und nur für Auflistung im Portal unter 'Verfahrensführende Behörden' benötigt.",
+        "application/json",
+        "json",
+        listOf("uvp")
+    )
+
+    override fun run(doc: Document, catalogId: String): Any {
+        return "{}"
+    }
+
+    override fun toString(exportedObject: Any): String {
+        return exportedObject.toString()
+    }
+}
