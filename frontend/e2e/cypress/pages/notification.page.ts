88e85b9ff5e8f2a5496f399de43db7da53044514
hani <hanidebs94@gmail.com>
2022-09-30 21:18:23 +0200

    Test: add new tests to test notifications management

--- frontend/e2e/cypress/pages/notification.page.ts
+++ frontend/e2e/cypress/pages/notification.page.ts
@@ -1,7 +1,7 @@
 export class NotificationPage {
   static visit() {
     cy.visit('settings/messages');
-    cy.get('ige-messages-management').contains('Benachrichtigungen', { timeout: 9000 });
+    cy.get('ige-messages-management', { timeout: 9000 }).contains('Benachrichtigungen', { timeout: 9000 });
   }
 
   static openNotificationDialog() {
@@ -15,13 +15,33 @@ export class NotificationPage {
   static toggleForGeneralNotification() {
     cy.get('mat-slide-toggle input').check({ force: true });
   }
+
+  static deleteNotification(notificationText: string) {
+    cy.contains('td', notificationText).siblings().contains('button', 'delete').click();
+    cy.get('mat-dialog-actions button').contains('Ok').click();
+  }
   static addNotification() {
     cy.intercept('POST', '/api/messages').as('incorrectAddNot');
     cy.get('[data-cy=create-notification]').click();
     cy.wait('@incorrectAddNot');
   }
 
-  static checkForNotificationInDashboard(notificationText: string) {
-    cy.get('.welcome .message').contains(notificationText);
+  static checkForNotificationInTable(notification: string, notContains: boolean = false) {
+    if (notContains) {
+      cy.get('ige-card-box').should('not.contain', notification);
+    } else {
+      cy.get('ige-card-box').should('contain', notification);
+    }
+  }
+
+  static setExpireDate(date: string) {
+    cy.get('.mat-datepicker-input').type(date);
+  }
+  static checkForNotificationInDashboard(notificationText: string, notContains: boolean = false) {
+    if (notContains) {
+      cy.get('.welcome ').should('not.contain', notificationText);
+    } else {
+      cy.get('.welcome ').should('contain', notificationText);
+    }
   }
 }
