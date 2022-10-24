55bd84a0654eeb10290f08881dd571855d10f00d
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2022-10-24 15:26:57 +0200

    Fix permission options for Organistaion types #4408
    (cherry picked from commit 2a1381f2db5ec2d6dcb5cf3b4afe99e250349923)

--- frontend/src/app/+user/permissions/permission-table/permission-table.component.ts
+++ frontend/src/app/+user/permissions/permission-table/permission-table.component.ts
@@ -102,7 +102,9 @@ export class PermissionTableComponent implements ControlValueAccessor {
     this.getDocument(doc.id).then((igeDoc) => {
       doc.hasWritePermission = igeDoc.hasWritePermission;
       doc.hasOnlySubtreeWritePermission = igeDoc.hasOnlySubtreeWritePermission;
-      doc.isFolder = igeDoc._type === "FOLDER";
+      // Organisations act like folders in this context and also have the hasOnlySubtreeWritePermission option
+      doc.isFolder =
+        igeDoc._type === "FOLDER" || igeDoc._type.endsWith("OrganisationDoc");
       doc.title = igeDoc.title;
 
       // downgrade permission if rights are not sufficient
