dc049d7776f695a243feaf93cc6a580fcb46b2db
André Wallat <andre.wallat@wemove.com>
2022-11-02 15:22:54 +0100

    Show drag-handle only when necessary (#3233)

--- frontend/src/app/formly/types/repeat/repeat.component.ts
+++ frontend/src/app/formly/types/repeat/repeat.component.ts
@@ -1,18 +1,29 @@
 import { Component, OnInit } from "@angular/core";
 import { FieldArrayType, FormlyFieldConfig } from "@ngx-formly/core";
 import { CdkDragDrop } from "@angular/cdk/drag-drop";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 
+@UntilDestroy()
 @Component({
   selector: "ige-repeat",
   templateUrl: "./repeat.component.html",
   styleUrls: ["./repeat.component.scss"],
 })
 export class RepeatComponent extends FieldArrayType implements OnInit {
+  canBeDragged = false;
+
   constructor() {
     super();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formControl.valueChanges
+      .pipe(untilDestroyed(this))
+      .subscribe((value) => {
+        this.canBeDragged =
+          this.formControl.enabled && !this.to.noDrag && value?.length > 1;
+      });
+  }
 
   drop(event: CdkDragDrop<FormlyFieldConfig>) {
     const item = this.model[event.previousIndex];
