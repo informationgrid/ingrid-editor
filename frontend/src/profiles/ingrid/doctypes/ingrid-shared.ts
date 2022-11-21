02eb71da1f6133b861accfc0c994e41ebf01f920
André Wallat <andre.wallat@wemove.com>
2022-11-21 23:36:31 +0100

    Revert setting additional id of form-group
    This ID also changes the resulting JSON of the form.

--- frontend/src/profiles/ingrid/doctypes/ingrid-shared.ts
+++ frontend/src/profiles/ingrid/doctypes/ingrid-shared.ts
@@ -41,7 +41,7 @@ export abstract class IngridShared extends BaseDoctype {
       "Allgemeines",
       [
         this.addGroup(
-          "info",
+          null,
           "Info",
           [
             this.addInputInline(
