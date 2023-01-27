a5a15dbeb24e78cac961829504c30cc42d718331
André Wallat <andre.wallat@wemove.com>
2023-01-27 15:02:12 +0100

    Fix corrupt map display (#4708)

--- frontend/src/app/formly/types/map/leaflet-type.component.ts
+++ frontend/src/app/formly/types/map/leaflet-type.component.ts
@@ -7,7 +7,7 @@ import {
   ViewChild,
 } from "@angular/core";
 import { FieldType } from "@ngx-formly/material";
-import { MapOptions, Rectangle } from "leaflet";
+import { Map, MapOptions, Rectangle } from "leaflet";
 import { ModalService } from "../../../services/modal/modal.service";
 import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
 import { MatDialog } from "@angular/material/dialog";
@@ -78,7 +78,10 @@ export class LeafletTypeComponent
         { ...options, scrollWheelZoom: false }
       );
 
-      // (<MyMap>this.leafletReference)._onResize();
+      // when switching from a folder to a document with leaflet map then we need
+      // to call resize event to prevent incorrect map display
+      // @ts-ignore
+      (<Map>this.leafletReference)._onResize();
       this.leafletReference.on("dragend", () => (this.mapHasMoved = true));
 
       this.locations = this.formControl.value || [];
