a18cfd1101303114b690ae92891e15e1a5391f48
André Wallat <andre.wallat@wemove.com>
2023-05-04 10:20:35 +0200

    Fix permission check when publishing a dataset (#5057)

--- server/src/main/java/de/ingrid/igeserver/services/DocumentService.kt
+++ server/src/main/java/de/ingrid/igeserver/services/DocumentService.kt
@@ -126,6 +126,9 @@ class DocumentService @Autowired constructor(
             val wrapper = docWrapperRepo.findById(id).get()
             val doc = docRepo.getByCatalogAndUuidAndIsLatestIsTrue(wrapper.catalog!!, wrapper.uuid)
             entityManager.detach(doc)
+            
+            // always add wrapper id which is needed when saving documents for authorization check
+            doc.wrapperId = id
 
             return if (expandReferences) DocumentData(
                 wrapper,
@@ -477,6 +480,7 @@ class DocumentService @Autowired constructor(
             val lastPublishedDoc =
                 getLastPublishedDocument(docData.document.catalog!!.identifier, docData.document.uuid)
             lastPublishedDoc.state = DOCUMENT_STATE.ARCHIVED
+            lastPublishedDoc.wrapperId = docData.wrapper.id
             docRepo.save(lastPublishedDoc)
         }
         docData.document.modified = dateService.now()
@@ -709,6 +713,7 @@ class DocumentService @Autowired constructor(
 
         val latestPublishedDoc = getLastPublishedDocument(catalogId, docData.wrapper.uuid)
         latestPublishedDoc.isLatest = true
+        latestPublishedDoc.wrapperId = id
         docRepo.save(latestPublishedDoc)
 
         // since we're within a transaction the expandInternalReferences-function would modify the db-document
@@ -736,6 +741,7 @@ class DocumentService @Autowired constructor(
         // remove publish
         val currentDoc = getDocumentFromCatalog(catalogId, id)
         val lastPublished = getLastPublishedDocument(catalogId, currentDoc.document.uuid)
+        lastPublished.wrapperId = id
 
         // run pre-unpublish pipe(s)
         val filterContext = DefaultContext.withCurrentProfile(catalogId, catalogRepo, principal)
@@ -779,6 +785,7 @@ class DocumentService @Autowired constructor(
         // if no draft version exists, move pending version to draft
         val updatedDoc = if (pendingDoc.isLatest) {
             pendingDoc.state = if (wasPublishedBefore) DOCUMENT_STATE.DRAFT_AND_PUBLISHED else DOCUMENT_STATE.DRAFT
+            pendingDoc.wrapperId = id
             docRepo.save(pendingDoc)
         } else {
             docData.document
