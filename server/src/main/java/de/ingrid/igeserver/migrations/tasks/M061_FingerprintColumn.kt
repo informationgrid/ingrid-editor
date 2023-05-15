3b71686f18f7d399c2e845991f6bf497dfab0b32
André Wallat <andre.wallat@wemove.com>
2023-05-15 18:33:02 +0200

    Add new column for fingerprints

---
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M061_FingerprintColumn.kt
@@ -0,0 +1,29 @@
+package de.ingrid.igeserver.migrations.tasks
+
+import de.ingrid.igeserver.migrations.MigrationBase
+import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
+import jakarta.persistence.EntityManager
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
+
+@Service
+class M061_FingerprintColumn : MigrationBase("0.61") {
+
+    @Autowired
+    lateinit var entityManager: EntityManager
+
+    @Autowired
+    private lateinit var transactionManager: PlatformTransactionManager
+
+    private val sqlUpdate = """
+        alter table document_wrapper add fingerprint jsonb;
+    """.trimIndent()
+
+    override fun exec() {
+        ClosableTransaction(transactionManager).use {
+            entityManager.createNativeQuery(sqlUpdate).executeUpdate()
+        }
+    }
+
+}
