b0d7b25003dacb09c552de31f90ecd921d0c4f53
André Wallat <andre.wallat@wemove.com>
2023-05-17 00:02:50 +0200

    Adapt tests

--- server/src/test/kotlin/de/ingrid/igeserver/exports/ingrid/Geodataset.kt
+++ server/src/test/kotlin/de/ingrid/igeserver/exports/ingrid/Geodataset.kt
@@ -6,6 +6,7 @@ import de.ingrid.igeserver.persistence.postgresql.jpa.model.ige.DocumentWrapper
 import de.ingrid.igeserver.profiles.ingrid.exporter.IngridIDFExporter
 import de.ingrid.igeserver.profiles.ingrid.exporter.IngridIndexExporter
 import de.ingrid.igeserver.profiles.ingrid.exporter.IngridLuceneExporter
+import de.ingrid.igeserver.repository.DocumentWrapperRepository
 import de.ingrid.igeserver.services.CatalogService
 import de.ingrid.igeserver.services.CodelistHandler
 import de.ingrid.igeserver.services.DocumentService
@@ -27,6 +28,7 @@ class Geodataset : AnnotationSpec() {
     private lateinit var exporter: IngridIDFExporter
     private lateinit var indexExporter: IngridIndexExporter
     private lateinit var luceneExporter: IngridLuceneExporter
+    private val documentWrapperRepository = mockk<DocumentWrapperRepository>()
     private val codelistHandler = mockk<CodelistHandler>()
     private val config = mockk<Config>()
 
@@ -34,7 +36,7 @@ class Geodataset : AnnotationSpec() {
     fun beforeAll() {
         this.exporter = IngridIDFExporter(codelistHandler, config, catalogService)
         this.luceneExporter = IngridLuceneExporter(codelistHandler, config, catalogService)
-        this.indexExporter = IngridIndexExporter(this.exporter, this.luceneExporter)
+        this.indexExporter = IngridIndexExporter(this.exporter, this.luceneExporter, documentWrapperRepository)
 
         mockCodelists(codelistHandler)
 
