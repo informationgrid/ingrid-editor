7304364340299d8c9aa40d92654a336256f55e00
André Wallat <andre.wallat@wemove.com>
2023-06-22 16:52:43 +0200

    Prevent copying identifier during copy operation (#5234)

--- server/src/main/java/de/ingrid/igeserver/profiles/ingrid/types/InGridGeoDatasetType.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/ingrid/types/InGridGeoDatasetType.kt
@@ -1,5 +1,6 @@
 package de.ingrid.igeserver.profiles.ingrid.types
 
+import de.ingrid.igeserver.persistence.postgresql.jpa.model.ige.Document
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -7,4 +8,11 @@ import org.springframework.stereotype.Component
 class InGridGeoDatasetType @Autowired constructor() : InGridBaseType() {
     override val className = "InGridGeoDataset"
     override val jsonSchema = "/ingrid/schemes/geo-dataset.schema.json"
+
+    override fun onCreate(doc: Document) {
+        super.onCreate(doc)
+        
+        // identifier must be empty, especially during copy operation (#5234)
+        doc.data.put("identifier", "")
+    }
 }
