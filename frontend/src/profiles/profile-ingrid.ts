907a9e01ad502f176846c26ddc45d128938c223d
André Wallat <andre.wallat@wemove.com>
2023-06-23 10:18:31 +0200

    Use general dialog template for conformity

--- frontend/src/profiles/profile-ingrid.ts
+++ frontend/src/profiles/profile-ingrid.ts
@@ -21,7 +21,6 @@ import { AsyncPipe, DatePipe, JsonPipe, NgForOf, NgIf } from "@angular/common";
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatSelectModule } from "@angular/material/select";
-import { TableDialogComponent } from "../app/shared/table-dialog/table-dialog.component";
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { GetCapabilitiesDialogComponent } from "../app/formly/types/update-get-capabilities/get-capabilities-dialog/get-capabilities-dialog.component";
 import { DialogTemplateModule } from "../app/shared/dialog-template/dialog-template.module";
@@ -76,7 +75,6 @@ class InGridComponent {
   declarations: [
     InGridComponent,
     ConformityDialogComponent,
-    TableDialogComponent,
     GetCapabilitiesDialogComponent,
     ThesaurusReportComponent,
   ],
