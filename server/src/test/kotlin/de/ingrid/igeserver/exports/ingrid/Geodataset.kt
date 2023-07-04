7288bef4f5817bad4a7a852495280d26a35360bc
Tsung-Wei Hsu <tsung-wei.hsu@wemove.com>
2023-07-04 16:06:45 +0200

    Add geo dataset unit test with samples (#5283)

--- server/src/test/kotlin/de/ingrid/igeserver/exports/ingrid/Geodataset.kt
+++ server/src/test/kotlin/de/ingrid/igeserver/exports/ingrid/Geodataset.kt
@@ -7,11 +7,13 @@ import de.ingrid.igeserver.profiles.ingrid.exporter.IngridIDFExporter
 import de.ingrid.igeserver.profiles.ingrid.exporter.IngridIndexExporter
 import de.ingrid.igeserver.profiles.ingrid.exporter.IngridLuceneExporter
 import de.ingrid.igeserver.repository.DocumentWrapperRepository
+import de.ingrid.igeserver.schema.SchemaUtils
 import de.ingrid.igeserver.services.CatalogService
 import de.ingrid.igeserver.services.CodelistHandler
 import de.ingrid.igeserver.services.DocumentService
 import de.ingrid.mdek.upload.Config
 import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.mockk.every
 import io.mockk.mockk
@@ -59,6 +61,21 @@ class Geodataset : AnnotationSpec() {
             // TODO: pending
             // result shouldBe SchemaUtils.getJsonFileContent("/export/ingrid/geodataset-Document1.idf.xml")
         }*/
+
+    @Test
+    fun maximalExport() {
+        every { documentService.getWrapperByDocumentId(any() as Int) } returns DocumentWrapper()
+
+        var result = exportJsonToXML(exporter, "/export/ingrid/geo-dataset.sample.maximal.json")
+        // replace generated UUIDs and windows line endings
+        result = result
+            .replace(GENERATED_UUID_REGEX, "ID_00000000-0000-0000-0000-000000000000")
+            .replace("\r\n", "\n")
+
+        result shouldNotBe null
+        // TODO: pending
+        // result shouldBe SchemaUtils.getJsonFileContent("/export/ingrid/geo-dataset.expected.maximal.idf.xml")
+    }
 
     @Test
     fun completeExport() {
