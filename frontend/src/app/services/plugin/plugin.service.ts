a25034711327834feede03a96c6f45a85daa2999
André Wallat <andre.wallat@wemove.com>
2023-07-31 16:09:19 +0200

    Fix plugin initialization

--- frontend/src/app/services/plugin/plugin.service.ts
+++ frontend/src/app/services/plugin/plugin.service.ts
@@ -52,6 +52,8 @@ export class PluginService {
   private initFormPlugins(forAddress: boolean) {
     this.initWithAddress = forAddress;
 
+    this.plugins.forEach((p) => p.setForAddress(forAddress));
+
     this.plugins
       .filter((p) => p.isActive)
       .filter((p) => !forAddress || (forAddress && !p.hideInAddress))
