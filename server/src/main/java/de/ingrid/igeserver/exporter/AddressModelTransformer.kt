cfcb2a9e5dc58c1f4a2f4bc9f1c6f649f2d56bd4
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2023-08-09 12:20:29 +0200

    Expand AddressModelTransformer

--- server/src/main/java/de/ingrid/igeserver/exporter/AddressModelTransformer.kt
+++ server/src/main/java/de/ingrid/igeserver/exporter/AddressModelTransformer.kt
@@ -4,30 +4,55 @@ import de.ingrid.igeserver.exporter.model.AddressModel
 import de.ingrid.igeserver.exporter.model.KeyValueModel
 
 class AddressModelTransformer(
-    val model: AddressModel,
+    private val model: AddressModel,
+    val catalogIdentifier: String,
     val codelist: CodelistTransformer,
     val type: KeyValueModel? = null,
 ) {
+    private var displayAddress: AddressModel
+    fun getModel() = displayAddress
+
+    init {
+        displayAddress = determineDisplayAddress()
+    }
 
     fun getIndividualName(): String? {
 
         // format: "lastName, firstName, salutation academicTitle"
-        val salutation = codelist.getValue("4300", model.salutation)
-        val academicTitle = codelist.getValue("4305", model.academicTitle)
+        val salutation = codelist.getValue("4300", displayAddress.salutation)
+        val academicTitle = codelist.getValue("4305", displayAddress.academicTitle)
         val namePostFix = listOfNotNull(salutation, academicTitle).joinToString(" ")
-        val individualName = listOfNotNull(model.lastName, model.firstName, namePostFix).joinToString(", ")
+        val individualName =
+            listOfNotNull(displayAddress.lastName, displayAddress.firstName, namePostFix).joinToString(", ")
 
         return individualName.ifBlank { null }
     }
 
-    val postBoxAddress = listOfNotNull( model.address.poBox, model.address.zipPoBox).joinToString(", ")
-    val country = model.address.country?.let { TransformationTools.getISO3166_1_Alpha_3FromNumericLanguageCode(it) }
-    val positionName = model.positionName
-    val zipCode = model.address.zipCode
-    val street = model.address.street
-    val city = model.address.city
-    val administrativeArea = codelist.getValue("110", model.address.administrativeArea)
-    val addressDocType = if (model.docType == "InGridOrganisationDoc") 0 else 1
+    private fun determineDisplayAddress(): AddressModel {
+        val nonHiddenAddress = model.getAncestorAddressesIncludingSelf(model.id, catalogIdentifier)
+
+        return if (nonHiddenAddress.size > 0) {
+            nonHiddenAddress.last()
+        } else model
+
+    }
+
+    val uuid = displayAddress.uuid
+    val namePresentation = displayAddress.getNamePresentation()
+    val hoursOfService = displayAddress.hoursOfService
+    val postBoxAddress = listOfNotNull(displayAddress.address.poBox, displayAddress.address.zipPoBox).joinToString(", ")
+    val country =
+        displayAddress.address.country?.let { TransformationTools.getISO3166_1_Alpha_3FromNumericLanguageCode(it) }
+    val positionName = displayAddress.positionName
+    val zipCode = displayAddress.address.zipCode
+    val zipPoBox = displayAddress.address.zipPoBox
+    val poBox = displayAddress.address.poBox
+    val street = displayAddress.address.street
+    val city = displayAddress.address.city
+    val email = displayAddress.email
+
+    val administrativeArea = codelist.getValue("110", displayAddress.address.administrativeArea)
+    val addressDocType = if (displayAddress.docType == "InGridOrganisationDoc") 0 else 2
 
 
 }
