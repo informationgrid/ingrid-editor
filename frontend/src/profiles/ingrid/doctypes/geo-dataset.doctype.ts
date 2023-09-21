8412acce1787c365cc85d47b0658ac9e3d1541ed
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2023-09-21 10:04:47 +0200

    Align display with export logic

--- frontend/src/profiles/ingrid/doctypes/geo-dataset.doctype.ts
+++ frontend/src/profiles/ingrid/doctypes/geo-dataset.doctype.ts
@@ -69,7 +69,7 @@ export class GeoDatasetDoctype extends IngridShared {
               const currentCatalog =
                 this.configService.$userInfo.value.currentCatalog;
               const namespace =
-                currentCatalog.settings.config?.namespace ??
+                this.addSuffix(currentCatalog.settings.config?.namespace) ??
                 `https://registry.gdi-de.org/id/${currentCatalog.id}/`;
               return value?.indexOf("://") >= 0
                 ? ""
@@ -502,5 +502,15 @@ export class GeoDatasetDoctype extends IngridShared {
       ],
       { fieldGroupClassName: "flex-row" }
     );
+  }
+
+  private addSuffix(url: string) {
+    if (!url) {
+      return url;
+    }
+    if (url?.endsWith("/") == true) {
+      return url;
+    }
+    return url + "/";
   }
 }
