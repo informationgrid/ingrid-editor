1474eb63e45edc1a1321f7d41a27ebddac1bb95b
Armin Arndt <armin.arndt@wemove.com>
2023-11-28 15:34:45 +0100

    Feature: Change CSW-T Endpoint
    - change endpoint to CSW style
    - add request params: SERVICE, REQUEST
    - edit docs

--- server/src/main/java/de/ingrid/igeserver/api/CswtApi.kt
+++ server/src/main/java/de/ingrid/igeserver/api/CswtApi.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*
 
 interface CswtApi {
 
-    @PostMapping(value = ["{collectionId}"], consumes = [MediaType.APPLICATION_XML_VALUE], produces = [MediaType.APPLICATION_XML_VALUE])
+    @PostMapping(value = ["/cswt"], consumes = [MediaType.APPLICATION_XML_VALUE], produces = [MediaType.APPLICATION_XML_VALUE])
     @Operation(tags=["CSW-T"], summary = "Insert, Update, Delete Records via CSW-t", hidden = false)
     @ApiResponses(value = [
         ApiResponse(responseCode = "200", description = "Successful operation."),
@@ -22,7 +22,9 @@ interface CswtApi {
         @Parameter(hidden = true) @RequestParam allRequestParams: Map<String, String>,
         @RequestHeader allHeaders: Map<String, String>,
         principal: Authentication,
-        @Parameter(description = "## Collection ID \n **OGC Parameter** \n\n The identifier for a specific record collection (i.e. catalogue identifier)." , required = true) @PathVariable("collectionId") collectionId: String,
+        @Parameter(description = "Value must be 'CSW'" , required = true) @RequestParam("SERVICE", defaultValue = "CSW") service: String = "CSW",
+        @Parameter(description = "Value must be 'Transaction'" , required = true ) @RequestParam("REQUEST", defaultValue = "Transaction") request: String,
+        @Parameter(description = "## Collection ID \n **OGC Parameter** \n\n The identifier for a specific record collection (i.e. catalogue identifier)." , required = true) @RequestParam("catalog") catalog: String,
         @Parameter(description = "The datasets to be inserted, delete or updated.", required = true) @RequestBody data: String,
         @Parameter(description = "## Dataset Folder ID \n **Custom Parameter** \n\n Add Dataset to Folder with UUID") @RequestParam(value = "datasetFolderId", required = false) datasetFolderId: String?,
         @Parameter(description = "## Address Folder ID \n **Custom Parameter** \n\n Add Address to Folder with UUID") @RequestParam(value = "addressFolderId", required = false) addressFolderId: String?,
