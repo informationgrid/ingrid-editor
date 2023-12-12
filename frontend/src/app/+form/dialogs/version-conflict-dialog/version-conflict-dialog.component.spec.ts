d57ebdb5b0fe2ac7113875c2072e50f7ecaea387
André Wallat <andre.wallat@wemove.com>
2023-12-12 11:44:03 +0100

    Adapt and add tests

--- frontend/src/app/+form/dialogs/version-conflict-dialog/version-conflict-dialog.component.spec.ts
+++ frontend/src/app/+form/dialogs/version-conflict-dialog/version-conflict-dialog.component.spec.ts
@@ -3,17 +3,45 @@ import { Spectator, createComponentFactory } from "@ngneat/spectator";
 import { VersionConflictDialogComponent } from "./version-conflict-dialog.component";
 import { MatDialogModule } from "@angular/material/dialog";
 import { SharedModule } from "../../../shared/shared.module";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
+import { ConfigService } from "../../../services/config/config.service";
 
 describe("VersionConflictDialogComponent", () => {
   let spectator: Spectator<VersionConflictDialogComponent>;
+  let configService;
+
   const createComponent = createComponentFactory({
     component: VersionConflictDialogComponent,
-    imports: [MatDialogModule, SharedModule],
+    imports: [MatDialogModule, SharedModule, HttpClientTestingModule],
   });
 
-  it("should create", () => {
+  beforeEach(() => {
+    configService = TestBed.inject(ConfigService);
+  });
+
+  it("should show two options when property not set", () => {
+    spyOn(configService, "getConfiguration").and.returnValue({});
     spectator = createComponent();
 
-    expect(spectator.component).toBeTruthy();
+    expect(spectator.queryAll("mat-radio-button").length).toEqual(2);
+  });
+
+  it("should show two options when property set to 'false'", () => {
+    spyOn(configService, "getConfiguration").and.returnValue({
+      allowOverwriteOnVersionConflict: false,
+    });
+    spectator = createComponent();
+
+    expect(spectator.queryAll("mat-radio-button").length).toEqual(2);
+  });
+
+  it("should show two options when property set to 'true'", () => {
+    spyOn(configService, "getConfiguration").and.returnValue({
+      allowOverwriteOnVersionConflict: true,
+    });
+    spectator = createComponent();
+
+    expect(spectator.queryAll("mat-radio-button").length).toEqual(3);
   });
 });
