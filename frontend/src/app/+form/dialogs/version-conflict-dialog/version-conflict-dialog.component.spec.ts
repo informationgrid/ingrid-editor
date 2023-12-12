de5a141f6e08434b349a2b6985bc927400cc7581
André Wallat <andre.wallat@wemove.com>
2023-12-12 11:47:39 +0100

    Adapt and add tests

--- frontend/src/app/+form/dialogs/version-conflict-dialog/version-conflict-dialog.component.spec.ts
+++ frontend/src/app/+form/dialogs/version-conflict-dialog/version-conflict-dialog.component.spec.ts
@@ -5,11 +5,16 @@ import { MatDialogModule } from "@angular/material/dialog";
 import { SharedModule } from "../../../shared/shared.module";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
-import { ConfigService } from "../../../services/config/config.service";
+import {
+  ConfigService,
+  Configuration,
+} from "../../../services/config/config.service";
 
 describe("VersionConflictDialogComponent", () => {
   let spectator: Spectator<VersionConflictDialogComponent>;
-  let configService;
+  let configService: ConfigService;
+  // @ts-ignore
+  const defaultConfig: Configuration = {};
 
   const createComponent = createComponentFactory({
     component: VersionConflictDialogComponent,
@@ -21,7 +26,9 @@ describe("VersionConflictDialogComponent", () => {
   });
 
   it("should show two options when property not set", () => {
-    spyOn(configService, "getConfiguration").and.returnValue({});
+    spyOn(configService, "getConfiguration").and.returnValue({
+      ...defaultConfig,
+    });
     spectator = createComponent();
 
     expect(spectator.queryAll("mat-radio-button").length).toEqual(2);
@@ -29,6 +36,7 @@ describe("VersionConflictDialogComponent", () => {
 
   it("should show two options when property set to 'false'", () => {
     spyOn(configService, "getConfiguration").and.returnValue({
+      ...defaultConfig,
       allowOverwriteOnVersionConflict: false,
     });
     spectator = createComponent();
@@ -38,6 +46,7 @@ describe("VersionConflictDialogComponent", () => {
 
   it("should show two options when property set to 'true'", () => {
     spyOn(configService, "getConfiguration").and.returnValue({
+      ...defaultConfig,
       allowOverwriteOnVersionConflict: true,
     });
     spectator = createComponent();
