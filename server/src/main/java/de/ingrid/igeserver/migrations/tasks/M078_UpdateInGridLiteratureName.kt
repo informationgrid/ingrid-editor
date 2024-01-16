2554e67970c6e2755148a02e33684e751fd9d16f
hani <hanidebs94@gmail.com>
2024-01-16 11:34:36 +0100

    Fix migration task conditions

--- server/src/main/java/de/ingrid/igeserver/migrations/tasks/M078_UpdateInGridLiteratureName.kt
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M078_UpdateInGridLiteratureName.kt
@@ -1,3 +1,22 @@
+/**
+ * ==================================================
+ * Copyright (C) 2023-2024 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 package de.ingrid.igeserver.migrations.tasks
 
 import com.fasterxml.jackson.databind.JsonNode
@@ -38,7 +57,6 @@ class M078_UpdateInGridLiteratureName : MigrationBase("0.78") {
 
     @Autowired
     private lateinit var docWrapper: DocumentWrapperRepository
-
     override fun exec() {}
 
     override fun postExec() {
@@ -47,12 +65,10 @@ class M078_UpdateInGridLiteratureName : MigrationBase("0.78") {
 
         ClosableTransaction(transactionManager).use {
             setAuthentication()
-
             log.info("Start migration of InGridLiteratureName")
-
             do {
                 log.info("Handling page $page")
-                val documentsAndWrappers = entityManager.createQuery(" SELECT dw, doc FROM DocumentWrapper dw, Document doc WHERE dw.uuid = doc.uuid  and dw.type = 'InGridPublication'  order by dw.uuid, doc.uuid ")
+                val documentsAndWrappers = entityManager.createQuery(" SELECT dw, doc FROM DocumentWrapper dw, Document doc WHERE dw.uuid = doc.uuid  and dw.type = 'InGridLiterature'  order by dw.uuid, doc.uuid ")
                     .setFirstResult((page - 1) * pageSize)
                     .setMaxResults(pageSize)
                     .resultList
@@ -60,11 +76,8 @@ class M078_UpdateInGridLiteratureName : MigrationBase("0.78") {
                 documentsAndWrappers
                     .map { it as Array<*> }  // Cast each result to an Array
                     .map {
-
                         val documentWrapper = it[0] as DocumentWrapper
                         val document = it[1] as Document
-
-                        // Pair them up or transform them as needed
                         Pair(documentWrapper, document)
                     }
                     .forEach { (documentWrapper, document) ->
@@ -76,15 +89,12 @@ class M078_UpdateInGridLiteratureName : MigrationBase("0.78") {
                             document.type = "InGridPublication"
                             log.info("Migrated doc  with dbID ${document.id}")
                             docRepo.save(document)
-
-
                         } catch (ex: Exception) {
                             log.error("Error migrating document with dbID ${documentWrapper.id}", ex)
                         }
                     }
                 page++
             } while (documentsAndWrappers.size == pageSize)
-
 
             page = 1
             do {
@@ -98,20 +108,15 @@ class M078_UpdateInGridLiteratureName : MigrationBase("0.78") {
                     .map { it as Query }
                     .forEach {
                         try {
-
-
-
                             val objectMapper = ObjectMapper()
                             val jsonString = it.data.toString()
-                            if (jsonString.contains("InGridPublication")) {
-                                val updatedJsonString = jsonString.replace("InGridPublication", "InGridPublication")
+                            if (jsonString.contains("InGridLiterature")) {
+                                val updatedJsonString = jsonString.replace("InGridLiterature", "InGridPublication")
                                 val updatedData: JsonNode = objectMapper.readTree(updatedJsonString)
                                 it.data = updatedData
                                 queryRepo.save(it)
                                 log.info("Migrated query with dbID ${it.id}")
                             }
-
-
                         } catch (ex: Exception) {
                             log.error("Error migrating query with dbID ${it.id}", ex)
                         }
@@ -121,10 +126,6 @@ class M078_UpdateInGridLiteratureName : MigrationBase("0.78") {
             } while (queries.size == pageSize)
         }
     }
-
-
-
-
     private fun setAuthentication() {
         val auth: Authentication =
             UsernamePasswordAuthenticationToken("Scheduler", "Task", listOf(SimpleGrantedAuthority("cat-admin")))
