75f709b983bf08509f99ad212dace199abf054a8
Tsung-Wei Hsu <tsungweihsu@Tsung-Weis-MacBook-Air.local>
2024-02-05 15:45:52 +0800

    Improve localization for accessibility (#4522)

---
+++ frontend/src/app/shared/ige-datepicker-intl.ts
@@ -0,0 +1,14 @@
+import { MatDatepickerIntl } from "@angular/material/datepicker";
+
+export class IgeDatepickerIntl extends MatDatepickerIntl {
+  openCalendarLabel = "Kalender öffnen";
+  closeCalendarLabel = "Kalender schließen";
+  switchToMultiYearViewLabel = "Zur Jahresansicht wechseln";
+  switchToMonthViewLabel = "Zur Monatsansicht wechseln";
+  prevMonthLabel = "Vorheriger Monat";
+  nextMonthLabel = "Nächster Monat";
+  prevYearLabel = "Vorheriges Jahr";
+  nextYearLabel = "Nächstes Jahr";
+  prevMultiYearLabel = "Vorherige 24 Jahre";
+  nextMultiYearLabel = "Nächste 24 Jahre";
+}
