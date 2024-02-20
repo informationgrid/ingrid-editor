f792d2b9d49ecc6380cfcf289bcba95fd8c3a27f
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2024-02-20 16:32:44 +0100

    Add copyright

--- server/src/main/java/de/ingrid/igeserver/profiles/ingrid_hmdk/importer/GeoserviceMapper.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/ingrid_hmdk/importer/GeoserviceMapper.kt
@@ -1,9 +1,26 @@
+/**
+ * ==================================================
+ * Copyright (C) 2024 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 package de.ingrid.igeserver.profiles.ingrid_hmdk.importer
 
 import de.ingrid.igeserver.exports.iso.Metadata
 import de.ingrid.igeserver.model.KeyValue
-import de.ingrid.igeserver.profiles.ingrid.importer.GeneralMapper
-import de.ingrid.igeserver.profiles.ingrid.importer.GeodatasetMapper
 import de.ingrid.igeserver.profiles.ingrid.importer.GeoserviceMapper
 import de.ingrid.igeserver.services.CodelistHandler
 
@@ -17,7 +34,6 @@ class GeoserviceMapperHMDK(metadata: Metadata, codeListService: CodelistHandler,
         return metadata.identificationInfo[0].identificationInfo?.descriptiveKeywords
             ?.filter { it.keywords?.thesaurusName?.citation?.title?.value == "HmbTG-Informationsgegenstand" }
             ?.flatMap { it.keywords?.keyword?.mapNotNull { it.value } ?: emptyList() }
-//            ?.map { codeListService.getCodelistEntry("informationsgegenstand", it) }
             ?.map { KeyValue(it) } ?: emptyList()
     }
 
