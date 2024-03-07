f5c6218535fa85bf8c08ceaade6e7ec873c00d79
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2024-03-07 10:25:09 +0100

    Add missing null check in codelist.pipe.ts

--- frontend/src/app/directives/codelist.pipe.ts
+++ frontend/src/app/directives/codelist.pipe.ts
@@ -34,11 +34,12 @@ export class CodelistPipe implements PipeTransform {
   ) {}
 
   transform(
-    value: string | BackendOption,
+    value: string | BackendOption | null,
     id: string,
     lang = "de",
   ): Observable<string> {
     if (!id) return of(value as string);
+    if (value === null) return of("");
     const codelist = this.codelistQuery.getEntity(id);
 
     if (!codelist) {
@@ -72,6 +73,7 @@ export class CodelistPipe implements PipeTransform {
     );
     if (entries.length === 1) {
       let fields = entries[0].fields;
+      // TODO: Refactor as it is too soon to set the default value for the DE field
       const hasDE = fields["de"];
       if (!hasDE) return { ...fields, de: fields["name"] };
       return fields;
