0aefdaeb13c2d6914fe7033900423519f15f3886
André Wallat <andre.wallat@wemove.com>
2024-04-15 09:26:31 +0200

    Add license header

--- server/src/main/java/de/ingrid/igeserver/profiles/ingrid_lfubayern/exporter/CommonTransformations.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/ingrid_lfubayern/exporter/CommonTransformations.kt
@@ -1,15 +1,36 @@
+/**
+ * ==================================================
+ * Copyright (C) 2024 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
 package de.ingrid.igeserver.profiles.ingrid_lfubayern.exporter
 
 import com.fasterxml.jackson.databind.JsonNode
 import de.ingrid.igeserver.exporter.model.CharacterStringModel
-import de.ingrid.igeserver.exports.iso.DescriptiveKeyword
 import de.ingrid.igeserver.profiles.ingrid.exporter.IngridModelTransformer.UseConstraintTemplate
 import de.ingrid.igeserver.profiles.ingrid.exporter.model.KeywordIso
 import de.ingrid.igeserver.profiles.ingrid.exporter.model.Thesaurus
 import de.ingrid.igeserver.utils.getString
 import de.ingrid.igeserver.utils.getStringOrEmpty
 
-fun lfubUseConstraints(superUseConstraints: List<UseConstraintTemplate>, docData: JsonNode): List<UseConstraintTemplate> {
+fun lfubUseConstraints(
+    superUseConstraints: List<UseConstraintTemplate>,
+    docData: JsonNode
+): List<UseConstraintTemplate> {
     val comments = docData.getString("resource.useConstraintsComments")
     if (comments.isNullOrEmpty()) return superUseConstraints
     return if (superUseConstraints.isNotEmpty()) {
