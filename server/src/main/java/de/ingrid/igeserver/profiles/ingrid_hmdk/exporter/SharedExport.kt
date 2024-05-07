339ce78fa41ffbcc22c76b2565051b612fbab614
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2024-05-07 11:26:20 +0200

    Add license header

--- server/src/main/java/de/ingrid/igeserver/profiles/ingrid_hmdk/exporter/SharedExport.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/ingrid_hmdk/exporter/SharedExport.kt
@@ -12,7 +12,7 @@
  * https://joinup.ec.europa.eu/software/page/eupl
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the Licence is distributed on an "AS IS" basis,F
+ * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
