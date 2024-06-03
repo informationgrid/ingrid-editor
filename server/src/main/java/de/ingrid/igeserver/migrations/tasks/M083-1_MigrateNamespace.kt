5f5415c4309efbc66eba96791471e2603f1ebc99
André Wallat <andre.wallat@wemove.com>
2024-06-03 15:06:05 +0200

    Migrate namespace to end with a slash
    This functionality was done automatically, but lead to problems where it wasn't necessary. So the slash must be added explicitly now, if wanted.
    
    (cherry picked from commit 74facfe888bf3e53e17e9b3f7a2d7f815abd9235)

---
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M083-1_MigrateNamespace.kt
@@ -0,0 +1,57 @@
+/**
+ * ==================================================
+ * Copyright (C) 2023-2024 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+package de.ingrid.igeserver.migrations.tasks
+
+import de.ingrid.igeserver.migrations.MigrationBase
+import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
+import de.ingrid.igeserver.services.CatalogService
+import jakarta.persistence.EntityManager
+import org.apache.logging.log4j.kotlin.logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
+
+@Service
+class `M083-1_MigrateNamespace` : MigrationBase("0.83.1") {
+    val log = logger()
+
+    @Autowired
+    lateinit var entityManager: EntityManager
+
+    @Autowired
+    lateinit var catalogService: CatalogService
+
+    @Autowired
+    private lateinit var transactionManager: PlatformTransactionManager
+
+    override fun exec() {
+        ClosableTransaction(transactionManager).use {
+            catalogService.getCatalogs()
+                .filter { it.type != "ingrid-lfubayern" }
+                .forEach {
+                    val namespace = it.settings.config.namespace
+                    if (namespace != null && !namespace.endsWith("/")) {
+                        it.settings.config.namespace += "/"
+                        catalogService.updateCatalog(it)
+                    }
+                }
+        }
+    }
+}
