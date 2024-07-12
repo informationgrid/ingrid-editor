c4c250059c867d15b8ecbdd69ca1e17ae56a1f8f
André Wallat <andre.wallat@wemove.com>
2024-07-12 17:13:23 +0200

    Fix getting uuid for OGC Export

--- server/src/main/java/de/ingrid/igeserver/features/ogc_api_records/export_record/OgcGeoJsonExporter.kt
+++ server/src/main/java/de/ingrid/igeserver/features/ogc_api_records/export_record/OgcGeoJsonExporter.kt
@@ -39,21 +39,22 @@ import org.springframework.stereotype.Service
 
 @Service
 class OgcGeoJsonExporter(
-        @Lazy val documentService: DocumentService,
-        val catalogService: CatalogService,
+    @Lazy val documentService: DocumentService,
+    val catalogService: CatalogService,
 ) : IgeExporter {
 
     override val typeInfo: ExportTypeInfo
         get() = ExportTypeInfo(
-                DocumentCategory.DATA,
-                "geojson",
-                "geoJsonIGE",
-                "geoJson Export des IGE",
-                MediaType.APPLICATION_JSON_VALUE,
-                "json",
-                listOf(),
-                false
+            DocumentCategory.DATA,
+            "geojson",
+            "geoJsonIGE",
+            "geoJson Export des IGE",
+            MediaType.APPLICATION_JSON_VALUE,
+            "json",
+            listOf(),
+            false
         )
+
     override fun run(doc: Document, catalogId: String, options: ExportOptions): Any {
         // TODO: move to utilities to prevent cycle
         val version = getRawJsonFromDocument(doc)
@@ -65,9 +66,9 @@ class OgcGeoJsonExporter(
         }
 
         val dataset = versions.first ?: versions.second
-        val isAddress: Boolean = if(dataset?.get("address") == null) false else true
-        if(isAddress) return dataset as Any
-        val record = addExportWrapper(catalogId, versions.first, versions.second)
+        val isAddress: Boolean = dataset?.get("address") != null
+        if (isAddress) return dataset as Any
+        val record = addExportWrapper(doc.uuid, versions.first, versions.second)
         return convertToJsonNode(record)
     }
 
@@ -81,7 +82,7 @@ class OgcGeoJsonExporter(
         }
     }
 
-    private fun addExportWrapper(catalogId: String, publishedVersion: JsonNode?, draftVersion: JsonNode?): Record {
+    private fun addExportWrapper(uuid: String, publishedVersion: JsonNode?, draftVersion: JsonNode?): Record {
 
         val dataset = publishedVersion ?: draftVersion
 
@@ -89,15 +90,15 @@ class OgcGeoJsonExporter(
         val interval: MutableList<String> = mutableListOf()
         interval.add("..") // start date
         interval.add("..") // end date
-        val time = RecordTime( interval, resolution = "P1D" )
+        val time = RecordTime(interval, resolution = "P1D")
 
         return Record(
-                id = dataset!!.get("_uuid"),
-                conformsTo = null,
-                type = "Feature",
-                time,
-                geometry,
-                properties = dataset
+            id = jacksonObjectMapper().createObjectNode().textNode(uuid),
+            conformsTo = null,
+            type = "Feature",
+            time,
+            geometry,
+            properties = dataset
         )
     }
 
@@ -107,8 +108,7 @@ class OgcGeoJsonExporter(
         mapper.registerModule(JavaTimeModule())
 
         val node = mapper.convertValue(record, ObjectNode::class.java)
-        val data = node
-        data.fields().forEach { entry ->
+        node.fields().forEach { entry ->
             node.replace(entry.key, entry.value)
         }
         return node
@@ -118,7 +118,6 @@ class OgcGeoJsonExporter(
     override fun toString(exportedObject: Any): String {
         return exportedObject as String
     }
-
 
 
 }
