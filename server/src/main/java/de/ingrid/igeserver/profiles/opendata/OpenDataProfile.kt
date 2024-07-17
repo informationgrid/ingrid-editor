16985845f64c687e5503e9525567a28e6dd6f1cf
André Wallat <andre.wallat@wemove.com>
2024-07-17 16:12:25 +0200

    Add specific default-mapping.json for opendata-profile

--- server/src/main/java/de/ingrid/igeserver/profiles/opendata/OpenDataProfile.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/opendata/OpenDataProfile.kt
@@ -178,7 +178,7 @@ class OpenDataProfile(
     }
 
     override fun getElasticsearchMapping(format: String): String {
-        return {}.javaClass.getResource("/ingrid/mappings/default-mapping.json")?.readText() ?: ""
+        return {}.javaClass.getResource("/opendata/mappings/default-mapping.json")?.readText() ?: ""
     }
 
     override fun getElasticsearchSetting(format: String): String {
