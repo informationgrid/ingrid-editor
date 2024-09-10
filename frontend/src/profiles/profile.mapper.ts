622c9068165982c67a514c4dd911eba547e6eed1
André Wallat <andre.wallat@wemove.com>
2024-09-10 11:41:10 +0200

    Adapt dynamic import of profile
    Angular recommends to use its new build system based on vite. This one cannot handle dynamic imports with non-static values. It's recommended to use static values, this is why we introduce a mapper function between the profile ID and the module of the profile.

---
+++ frontend/src/profiles/profile.mapper.ts
@@ -0,0 +1,34 @@
+import { from } from "rxjs";
+
+export function ProfileMapper(profileId: string) {
+  switch (profileId) {
+    case "ingrid":
+      return from(import("./profile-ingrid"));
+    case "uvp":
+      return from(import("./profile-uvp"));
+    case "bmi":
+      return from(import("./profile-bmi"));
+    case "ingrid-bast":
+      return from(import("./profile-ingrid-bast"));
+    case "ingrid-hmdk":
+      return from(import("./profile-ingrid-hmdk"));
+    case "ingrid-kommunal-st":
+      return from(import("./profile-ingrid-kommunal-st"));
+    case "ingrid-krzn":
+      return from(import("./profile-ingrid-krzn"));
+    case "ingrid-lfubayern":
+      return from(import("./profile-ingrid-lfubayern"));
+    case "ingrid-lubw":
+      return from(import("./profile-ingrid-lubw"));
+    case "ingrid-up-sh":
+      return from(import("./profile-ingrid-up-sh"));
+    case "ingrid-wsv":
+      return from(import("./profile-ingrid-wsv"));
+    case "mcloud":
+      return from(import("./profile-mcloud"));
+    case "opendata":
+      return from(import("./profile-opendata"));
+    case "test":
+      return from(import("./profile-test"));
+  }
+}
