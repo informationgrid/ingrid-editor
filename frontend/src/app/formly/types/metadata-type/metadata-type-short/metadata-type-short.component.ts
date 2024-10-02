99c485b54bdfd0a346e5839a3645613732969a10
André Wallat <andre.wallat@wemove.com>
2024-10-02 16:32:20 +0200

    Add styling and improvements

--- frontend/src/app/formly/types/metadata-type/metadata-type-short/metadata-type-short.component.ts
+++ frontend/src/app/formly/types/metadata-type/metadata-type-short/metadata-type-short.component.ts
@@ -1,13 +1,14 @@
 import { Component, input } from "@angular/core";
-import { MatChip, MatChipSet } from "@angular/material/chips";
+import { MatChipListbox, MatChipOption } from "@angular/material/chips";
 import { MetadataOption } from "../metadata-type.component";
 import { MatButton } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
+import { JsonPipe } from "@angular/common";
 
 @Component({
   selector: "ige-metadata-type-short",
   standalone: true,
-  imports: [MatChipSet, MatChip, MatButton, MatIcon],
+  imports: [MatButton, MatIcon, MatChipOption, MatChipListbox, JsonPipe],
   templateUrl: "./metadata-type-short.component.html",
   styleUrl: "./metadata-type-short.component.scss",
 })
