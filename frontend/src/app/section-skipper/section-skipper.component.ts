2c42a4f52522565e655947b5e92a995fcc60711c
Tsung-Wei Hsu <tsungweihsu29@gmail.com>
2024-11-12 14:51:19 +0100

    Build section skipper for accessibility(#4464)

---
+++ frontend/src/app/section-skipper/section-skipper.component.ts
@@ -0,0 +1,46 @@
+/**
+ * ==================================================
+ * Copyright (C) 2023-2024 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "ige-section-skipper",
+  templateUrl: "./section-skipper.component.html",
+  styleUrls: ["./section-skipper.component.scss"],
+  standalone: true,
+})
+export class SectionSkipperComponent {
+  focusable = "a, button, [tabindex='0']";
+
+  skipToMainMenu() {
+    this.getFocusable("ige-side-menu")?.focus();
+  }
+
+  skipToMainHeader() {
+    this.getFocusable("ige-main-header")?.focus();
+  }
+
+  skipToMainContent() {
+    this.getFocusable("ige-init-catalog")?.focus();
+  }
+
+  private getFocusable(section: string): HTMLElement | undefined {
+    return document.querySelector(section)?.querySelector(this.focusable);
+  }
+}
