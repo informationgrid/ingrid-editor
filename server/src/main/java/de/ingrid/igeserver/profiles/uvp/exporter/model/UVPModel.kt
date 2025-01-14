d826ff40eb8a04b066b1474b858b6b9e4e41b9d9
André Wallat <andre.wallat@wemove.com>
2025-01-14 10:50:33 +0100

    Reverse order of address parents in UVP profile (#6021)
    Previously, the order of ancestor addresses was not reversed. This update ensures the list of parent addresses is returned in reversed order after dropping the last entry, aligning with expected behavior.
    
    (cherry picked from commit 917935e7ce718ab1e01ea9fa5262d3f244996611)

--- server/src/main/java/de/ingrid/igeserver/profiles/uvp/exporter/model/UVPModel.kt
+++ server/src/main/java/de/ingrid/igeserver/profiles/uvp/exporter/model/UVPModel.kt
@@ -221,7 +221,7 @@ data class UVPModel(
 
     fun hasPoBox(): Boolean = !pointOfContact?.poBox.isNullOrEmpty()
 
-    fun getUvpAddressParents(): List<DocumentData> = if (pointOfContact!!.parentAddresses.isEmpty()) emptyList() else nonHiddenAncestorAddresses!!.dropLast(1)
+    fun getUvpAddressParents(): List<DocumentData> = if (pointOfContact!!.parentAddresses.isEmpty()) emptyList() else nonHiddenAncestorAddresses!!.dropLast(1).reversed()
 
     fun getUvpAddressParentsIncludingCurrent(): List<AddressShort> = if (pointOfContact == null) emptyList() else nonHiddenAncestorAddresses!!.map { getAddressShort(it.document) }
 
