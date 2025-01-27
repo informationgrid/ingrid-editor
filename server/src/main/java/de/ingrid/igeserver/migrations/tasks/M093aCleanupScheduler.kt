9c7b2227f401a3bbd7809434521f9de25932981a
André Wallat <andre.wallat@wemove.com>
2025-01-27 11:30:22 +0100

    Remove duplicate cron triggers
    This migration task cleans up the scheduler by deleting redundant cron triggers caused by the separation of cron-trigger and manual indexing. It ensures proper indexing behavior by removing unnecessary entries from the database.
    
    (cherry picked from commit 16a8f2fe2cdcf3f2ecdca9f24ae68d7002f34740)

---
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M093aCleanupScheduler.kt
@@ -0,0 +1,57 @@
+/**
+ * ==================================================
+ * Copyright (C) 2025 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+package de.ingrid.igeserver.migrations.tasks
+
+import de.ingrid.igeserver.migrations.MigrationBase
+import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
+import de.ingrid.igeserver.utils.setAdminAuthentication
+import jakarta.persistence.EntityManager
+import org.apache.logging.log4j.kotlin.logger
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.PlatformTransactionManager
+
+/**
+ * Cleanup scheduler after separating cron-trigger from manual indexing. This lead to duplicate triggers and wrong indexing.
+ */
+@Service
+class M093aCleanupScheduler : MigrationBase("0.93.1") {
+
+    private var log = logger()
+
+    @Autowired
+    lateinit var entityManager: EntityManager
+
+    @Autowired
+    private lateinit var transactionManager: PlatformTransactionManager
+
+    override fun exec() {}
+
+    override fun postExec() {
+        ClosableTransaction(transactionManager).use {
+            setAdminAuthentication("Migration", "Task")
+            val removed = entityManager.createNativeQuery("""DELETE FROM qrtz_cron_triggers WHERE trigger_name='index'""")
+                .executeUpdate()
+            entityManager.createNativeQuery("""DELETE FROM qrtz_triggers WHERE trigger_name='index'""")
+                .executeUpdate()
+            log.info("""$removed cron triggers removed""")
+        }
+    }
+}
