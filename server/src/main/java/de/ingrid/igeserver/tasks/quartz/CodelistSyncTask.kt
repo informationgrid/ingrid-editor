cd08ff69cc21dd5f50dbcb020f2e73c0ae81ca3f
André Wallat <andre.wallat@wemove.com>
2025-04-30 13:39:44 +0200

    Apply codelist-id to each codelist-item in a dataset

--- server/src/main/java/de/ingrid/igeserver/tasks/quartz/CodelistSyncTask.kt
+++ server/src/main/java/de/ingrid/igeserver/tasks/quartz/CodelistSyncTask.kt
@@ -21,8 +21,6 @@ package de.ingrid.igeserver.tasks.quartz
 
 import de.ingrid.igeserver.api.messaging.Message
 import de.ingrid.igeserver.services.CatalogService
-import de.ingrid.igeserver.services.CodelistField
-import de.ingrid.igeserver.services.CodelistHandler
 import org.apache.logging.log4j.kotlin.logger
 import org.quartz.JobExecutionContext
 import org.quartz.PersistJobDataAfterExecution
@@ -31,11 +29,6 @@ import org.springframework.stereotype.Component
 import java.util.*
 
 data class FieldToCodelist(
-    val field: String?,
-    val codelist: String,
-)
-
-data class ArrayFieldToCodelist(
     val arrayField: String? = null,
     val subField: String?,
     val codelist: String,
@@ -45,7 +38,6 @@ data class ArrayFieldToCodelist(
 @PersistJobDataAfterExecution
 class CodelistSyncTask(
     private val jdbcTemplate: JdbcTemplate,
-    private val codelistHandler: CodelistHandler,
     private val catalogService: CatalogService,
 ) : IgeJob() {
 
@@ -61,13 +53,47 @@ class CodelistSyncTask(
         val catalogLanguage = catalog.settings.config.language ?: "de"
         val profile = catalogService.getCatalogProfile(catalog.type)
 
-        profile.codelistFields.forEach {
+        val fields = listOf(
+            FieldToCodelist("advProductGroups", null, "8010"),
+            FieldToCodelist("spatial.spatialSystems", null, "100"),
+//          FieldToCodelistld("gridSpatialRepresentation.type", ""),
+            FieldToCodelist("distribution.format", "name", "1320"),
+            FieldToCodelist(null, "fileReferences.format", "1320"),
+            FieldToCodelist("themes", null, "6100"),
+            FieldToCodelist("openDataCategories", null, "6400"),
+            FieldToCodelist("hvdCategories", null, "hvdCategories"),
+            FieldToCodelist("priorityDatasets", null, "6350"),
+            FieldToCodelist(null, "spatialScope", "6360"),
+            FieldToCodelist("topicCategories", null, "527"),
+            FieldToCodelist(null, "spatial.verticalExtent.unitOfMeasure", "102"),
+            FieldToCodelist(null, "spatial.verticalExtent.Datum", "101"),
+            FieldToCodelist("temporal.events", "referenceDateType", "502"),
+            FieldToCodelist(null, "temporal.status", "523"),
+            FieldToCodelist(null, "maintenanceInformation.maintenanceAndUpdateFrequency", "518"),
+            FieldToCodelist(null, "maintenanceInformation.userDefinedMaintenanceFrequency.unit", "1230"),
+            FieldToCodelist(null, "metadata.language", "99999999"),
+            FieldToCodelist("dataset.languages", null, "99999999"),
+            FieldToCodelist(null, "metadata.characterSet", "510"),
+            FieldToCodelist("conformanceResult", "pass", "6000"),
+            FieldToCodelist("conformanceResult", "specification", "6005"),
+            FieldToCodelist("extraInfo.legalBasicsDescriptions", null, "1350"),
+            FieldToCodelist("resource.accessConstraints", null, "6010"),
+            FieldToCodelist("resource.useConstraints", "title", "6500"),
+            FieldToCodelist("digitalTransferOptions", "name", "520"),
+            FieldToCodelist(null, "generalResourceType", "3390"),
+            FieldToCodelist(null, "resourceType", "3386"),
+            FieldToCodelist("references", "type", "2000"),
+            FieldToCodelist("references", "urlDataType", "1320"),
+            FieldToCodelist("pointOfContact", "type", "505"),
+            FieldToCodelist(null, "service.type", "5100"),
+            FieldToCodelist("service.classification", null, "5200"),
+            FieldToCodelist("service.version", null, "5152"), // dynamic!!!
+            FieldToCodelist("service.operations", "name", "5110"), // dynamic!!!
+        )
+        fields.forEach {
             try {
                 // Execute the SQL query to update codelist values
-                val sql = when (it) {
-                    is CodelistField.ListField -> getSQL(it, catalogIdentifier, catalogLanguage)
-                    is CodelistField.SingleField -> getSQLForObject(it, catalogIdentifier, catalogLanguage)
-                }
+                val sql = getSQL(it, catalogIdentifier, catalogLanguage)
                 log.info("Executing SQL: $sql")
                 val updatedRows = jdbcTemplate.update(sql)
 
@@ -83,86 +109,52 @@ class CodelistSyncTask(
         finishJob(context, message)
     }
 
-    private fun getSQL(codelistField: CodelistField.ListField, catalogIdentifier: String, language: String): String {
-        val codelist = codelistHandler.getCodelists(listOf(codelistField.codelist)).firstOrNull()
-            ?: codelistHandler.getCatalogCodelists(catalogIdentifier).find { it.id == codelistField.codelist }
-        val fieldPath = if (codelistField.subField != null) {
-            "-> " + codelistField.subField.split(".")
-                .joinToString(" -> ") { "'$it'" }
-        } else {
-            ""
-        }
-        val generatedWhens = codelist?.entries?.map {
-            """WHEN elem $fieldPath ->> 'key' = '${it.id}' THEN '${it.fields[language]}'"""
-        }
-        if (generatedWhens.isNullOrEmpty()) throw IllegalArgumentException("No codelist found for key ${codelistField.codelist}")
-
-        val arrayField = codelistField.arrayField!!
-        val jsonPath = arrayField.split(".").joinToString(" -> ") { "'$it'" }
-        val jsonPath2 = arrayField.replace(".", ",")
-        val valueField = codelistField.subField?.replace(".", ",")?.let { "$it,value" } ?: "value"
+    private fun getSQL(codelistField: FieldToCodelist, catalogIdentifier: String, language: String): String {
+        val rootField = codelistField.arrayField ?: codelistField.subField!!
+        val jsonPath = rootField.split(".").joinToString(" -> ") { "'$it'" }
+        val jsonPath2 = rootField.replace(".", ",")
+        val nullFilter = rootField
+        val subPathForArrays = codelistField.subField?.replace(".", ",")?.let { "$it," } ?: ""
         return """
                 UPDATE document d
                 SET data = jsonb_set(
                         d.data,
                         '{$jsonPath2}',
-                        (
-                            SELECT jsonb_agg(
-                                           CASE
-                                               WHEN elem $fieldPath ? 'key' THEN
-                                                   jsonb_set(
-                                                           elem,
-                                                           '{$valueField}',
-                                                           to_jsonb(
-                                                                   CASE
-                                                                       ${generatedWhens.joinToString("\n")}
-                                                                       ELSE NULL
-                                                                       END
-                                                           ),
-                                                           TRUE
-                                                   )
-                                               ELSE elem
-                                           END
-                                   )
-                            FROM jsonb_array_elements(d.data -> $jsonPath) elem
-                        ),
-                        TRUE
+                        CASE
+                            WHEN jsonb_typeof(d.data -> $jsonPath) = 'array' THEN
+                                COALESCE(
+                                    (
+                                        SELECT jsonb_agg(
+                                            jsonb_set(elem, '{${subPathForArrays}_codelistId}', '"${codelistField.codelist}"', TRUE)
+                                        )
+                                        FROM jsonb_array_elements(d.data -> $jsonPath) elem
+                                    ),
+                                    '[]'::jsonb
+                                )
+                            WHEN jsonb_typeof(d.data -> $jsonPath) = 'object' THEN
+                                jsonb_set(d.data -> $jsonPath, '{_codelistId}', '"${codelistField.codelist}"', TRUE)
+                            END,
+                        FALSE
                            )
                 FROM document_wrapper dw JOIN catalog cat ON dw.catalog_id = cat.id
                 WHERE d.uuid = dw.uuid
                   AND cat.identifier = '$catalogIdentifier'
                   AND dw.deleted = 0
                   AND d.state != 'ARCHIVED'
-                  AND jsonb_typeof(d.data -> $jsonPath) = 'array'
+                  AND d.data ${convertToJsonPathForNullCheck(rootField)} IS NOT NULL
         """.trimIndent()
     }
 
-    private fun getSQLForObject(codelistField: CodelistField.SingleField, catalogIdentifier: String, language: String): String {
-        val codelist = codelistHandler.getCodelists(listOf(codelistField.codelist)).firstOrNull()
-            ?: codelistHandler.getCatalogCodelists(catalogIdentifier).find { it.id == codelistField.codelist }
-        val jsonPath = codelistField.field!!.split(".").joinToString(" -> ") { "'$it'" }
-        val generatedWhens = codelist?.entries?.map {
-            """WHEN d.data -> $jsonPath ->> 'key' = '${it.id}' THEN jsonb_set(d.data -> $jsonPath, '{value}', '"${it.fields[language]}"', TRUE)"""
-        }
-        if (generatedWhens.isNullOrEmpty()) throw IllegalArgumentException("No codelist found for key ${codelistField.codelist}")
+    private fun convertToJsonPathForNullCheck(dotPath: String): String {
+        val parts = dotPath.split(".")
 
-        return """
-                UPDATE document d
-                SET data = jsonb_set(
-                        d.data,
-                        '{${codelistField.field.replace(".", ",")}}',
-                        CASE
-                            ${generatedWhens.joinToString("\n")}
-                            ELSE d.data -> $jsonPath
-                        END,
-                        TRUE
-                           )
-                FROM document_wrapper dw JOIN catalog cat ON dw.catalog_id = cat.id
-                WHERE d.uuid = dw.uuid
-                  AND cat.identifier = 'test_catalog'
-                  AND dw.deleted = 0
-                  AND d.state != 'ARCHIVED'
-                  AND jsonb_typeof(d.data -> $jsonPath) = 'object';
-        """.trimIndent()
+        return parts.mapIndexed { index, part ->
+            when {
+                // Letztes Element mit ->>
+                index == parts.size - 1 -> " ->> '$part'"
+                // Mittlere Elemente mit ->
+                else -> " -> '$part'"
+            }
+        }.joinToString("")
     }
 }
--- server/src/test/kotlin/de/ingrid/igeserver/tasks/quartz/CodelistSyncTaskTest.kt
+++ server/src/test/kotlin/de/ingrid/igeserver/tasks/quartz/CodelistSyncTaskTest.kt
@@ -122,6 +122,56 @@ class CodelistSyncTaskTest : IntegrationTest() {
             }
     }
 
+    @Test
+    fun `migrate codelist ids inside document`() {
+        // Execute the task
+        codelistSyncTask.run(jobExecutionContext)
+
+        // Verify that each call to jdbcTemplate.update returns 1 updated row
+        /*verify(exactly = 27) {
+            val result = jdbcTemplate.update(any() as String)
+            assert(result == 1) { "Expected sql result to return 1 updated rows, but got $result" }
+        }*/
+
+        entityManager.createNativeQuery(
+            "SELECT data FROM document WHERE id = 1001",
+            JsonNode::class.java,
+        ).resultList.first()
+            .let {
+                it as JsonNode
+                println(it)
+                it.get("themes").get(0).getString("_codelistId") shouldBe "6100"
+                it.get("advProductGroups").get(0).getString("_codelistId") shouldBe "8010"
+                it.getPath("service.version")!!.get(0).getString("_codelistId") shouldBe "5152"
+                it.getPath("service.operations")!!.get(0).getString("name._codelistId") shouldBe "5110"
+                it.getPath("service.classification")!!.get(0).getString("_codelistId") shouldBe "5200"
+                it.getPath("spatial.spatialSystems")!!.get(0).getString("_codelistId") shouldBe "100"
+                it.getString("service.type._codelistId") shouldBe "5100"
+                it.getString("spatial.verticalExtent.Datum._codelistId") shouldBe "101"
+                it.getString("spatial.verticalExtent.unitOfMeasure._codelistId") shouldBe "102"
+                it.getString("metadata.language._codelistId") shouldBe "99999999"
+                it.getPath("resource.useConstraints")!!.get(0).getString("title._codelistId") shouldBe "6500"
+                it.getPath("resource.accessConstraints")!!.get(0).getString("_codelistId") shouldBe "6010"
+                it.getPath("temporal.events")!!.get(0).getString("referenceDateType._codelistId") shouldBe "502"
+                it.getString("temporal.status._codelistId") shouldBe "523"
+//                it.getString("temporal.resourceDateType._codelistId") shouldBe "???"
+                it.getPath("references")!!.get(0).getString("type._codelistId") shouldBe "2000"
+                it.getPath("references")!!.get(0).getString("urlDataType._codelistId") shouldBe "1320"
+                it.getPath("distribution.format")!!.get(0).getString("name._codelistId") shouldBe "1320"
+                it.getString("spatialScope._codelistId") shouldBe "6360"
+                it.getPath("pointOfContact")!!.get(0).getString("type._codelistId") shouldBe "505"
+                it.getPath("pointOfContact")!!.get(1).getString("type._codelistId") shouldBe "505"
+                it.getPath("priorityDatasets")!!.get(0).getString("_codelistId") shouldBe "6350"
+                it.getPath("conformanceResult")!!.get(0).getString("pass._codelistId") shouldBe "6000"
+                it.getPath("conformanceResult")!!.get(0).getString("specification._codelistId") shouldBe "6005"
+                it.getPath("digitalTransferOptions")!!.get(0).getString("name._codelistId") shouldBe "520"
+//                it.getPath("digitalTransferOptions")!!.get(0).getString("transferSize.unit._codelistId") shouldBe "???"
+                it.getString("maintenanceInformation.maintenanceAndUpdateFrequency._codelistId") shouldBe "518"
+                it.getString("maintenanceInformation.userDefinedMaintenanceFrequency.unit._codelistId") shouldBe "1230"
+                it.getPath("extraInfo.legalBasicsDescriptions")!!.get(0).getString("_codelistId") shouldBe "1350"
+            }
+    }
+
 //    @Test
     /*
         fun `test error handling`() {
