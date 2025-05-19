c252f2f3870208a959923e8fbfbba211a632c77d
André Wallat <andre.wallat@wemove.com>
2025-05-19 11:31:14 +0200

    Fix sorting table when group is null

--- frontend/src/profiles/ingrid-lubw/doctypes/geo-dataset.doctype.ts
+++ frontend/src/profiles/ingrid-lubw/doctypes/geo-dataset.doctype.ts
@@ -306,10 +306,10 @@ export class GeoDatasetDoctypeLubwSkdvOk extends GeoDatasetDoctype {
   ) {
     if (!isNew) {
       if (
-        (item[field].key !== null &&
-          item[field].key === allData[currentIndex][field]?.key) ||
-        (item[field].value !== null &&
-          item[field].value === allData[currentIndex][field]?.value)
+        (item[field]?.key !== null &&
+          item[field]?.key === allData[currentIndex][field]?.key) ||
+        (item[field]?.value !== null &&
+          item[field]?.value === allData[currentIndex][field]?.value)
       ) {
         allData.splice(currentIndex, 1, item);
         return;
