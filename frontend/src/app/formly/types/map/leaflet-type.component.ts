9f37272cd6e894bc2f273bfc9c7cfdef3882706b
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2025-06-03 15:37:34 +0200

    Update leaflet-type.component.ts to work after merge

--- frontend/src/app/formly/types/map/leaflet-type.component.ts
+++ frontend/src/app/formly/types/map/leaflet-type.component.ts
@@ -96,13 +96,8 @@ export class LeafletTypeComponent
     this.leaflet.nativeElement.style.width = "100%";
 
     this.formControl.valueChanges
-      .pipe(
-        untilDestroyed(this),
-        debounceTime(0),
-        distinctUntilChanged(),
-        tap((value: SpatialLocation[]) => (this.locations = value || [])),
-      )
-      .subscribe(() => this.updateBoundingBoxCatchingErrors());
+      .pipe(untilDestroyed(this), debounceTime(0), distinctUntilChanged())
+      .subscribe((value) => this.updateBoundingBox(value || []));
 
     try {
       const options: MapOptions = this.props.mapOptions;
@@ -119,20 +114,21 @@ export class LeafletTypeComponent
 
       // this.locations = this.formControl.value || [];
       // delay update to prevent template error because of 'hasAnyLocations' update
-      setTimeout(() => this.updateBoundingBoxCatchingErrors());
+      /*setTimeout(() => {
+        try {
+          this.updateBoundingBox();
+        } catch (e) {
+          console.warn(
+            "Failed to update bounding box. map already unloaded?",
+            e,
+          );
+        }
+      });*/
     } catch (e) {
       console.error("Problem initializing the map component.", e);
       this.updateLocations([]);
       this.formControl.setValue([]);
       throw Error("Problem initializing the map component: " + e.message);
-    }
-  }
-
-  private updateBoundingBoxCatchingErrors() {
-    try {
-      this.updateBoundingBox();
-    } catch (e) {
-      console.warn("Failed to update bounding box. map already unloaded?", e);
     }
   }
 
