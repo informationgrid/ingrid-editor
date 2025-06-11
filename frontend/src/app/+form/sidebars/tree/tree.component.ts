82237f3eea70569477f7ae94f1044013eb242922
André Wallat <andre.wallat@wemove.com>
2025-06-11 10:51:57 +0200

    Fix setting the active node in tree
    The active node was not set immediately so that it could come to a race condition when the tree needs to be reloaded.

--- frontend/src/app/+form/sidebars/tree/tree.component.ts
+++ frontend/src/app/+form/sidebars/tree/tree.component.ts
@@ -176,9 +176,7 @@ export class TreeComponent implements OnInit {
         // delay reload to use correct activeId
         // when we jump from import page (after an import) the previously active node
         // could be opened instead of the imported document
-        setTimeout(() => this.reloadTree(true).subscribe(), 500);
-        // console.log("active tree node before reload: ", this.activeNodeId());
-        // this.reloadTree(true).subscribe();
+        setTimeout(() => this.reloadTree(true).subscribe(), 300);
       }
     });
   }
@@ -250,10 +248,10 @@ export class TreeComponent implements OnInit {
     this.setActiveNode
       .pipe(untilDestroyed(this), debounceTime(100), distinctUntilChanged())
       .subscribe(async (id) => {
-        console.log("active node changed: ", id);
         if (this.activeNodeId() === id) {
           return;
         }
+        this.activeNodeId.set(id);
         // when setting a node from the outside, then do not emit activate event again
         this.jumpToNode(id, true, false).catch((e) => this.error.next(e));
       });
@@ -306,7 +304,6 @@ export class TreeComponent implements OnInit {
   }
 
   reloadTree(forceFromServer = false): Observable<TreeNode[]> {
-    console.log("Reloading tree", this.activeNodeId(), forceFromServer);
     return this.database.initialData(forceFromServer, this.forAddresses).pipe(
       map((docs) => this.database.mapDocumentsToTreeNodes(docs, 0)),
       map((docs) => docs.sort(this.treeService.getSortTreeNodesFunction())),
