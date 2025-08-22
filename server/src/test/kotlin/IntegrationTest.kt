e9355cefdd72aa93e14cdaac6c5a7f98d1b05922
André Wallat <andre.wallat@wemove.com>
2025-08-22 11:37:52 +0200

    Fix codelist-id migration for LfUBayern (#8082)

--- server/src/test/kotlin/IntegrationTest.kt
+++ server/src/test/kotlin/IntegrationTest.kt
@@ -35,7 +35,7 @@ import org.springframework.test.context.jdbc.SqlConfig
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Sql(scripts = ["/test_data_acl.sql"], config = SqlConfig(encoding = "UTF-8"))
 @AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles(profiles = ["default", "uvp", "ogc-api", "ingrid", "mcloud", "ingrid-krzn", "opendata", "ingrid-hmdk"])
+@ActiveProfiles(profiles = ["default", "uvp", "ogc-api", "ingrid", "mcloud", "ingrid-krzn", "opendata", "ingrid-hmdk", "ingrid-lfubayern"])
 class IntegrationTest : AnnotationSpec() {
     override fun extensions() = listOf(SpringExtension)
 
