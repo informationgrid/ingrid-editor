90f68aa180b8674a5e83c0a5e9ef1178540952eb
André Wallat <andre.wallat@wemove.com>
2025-09-24 10:09:39 +0200

    Remove unused @Profile annotation from migration task M085

--- server/src/main/java/de/ingrid/igeserver/migrations/tasks/M085AddIndicesForParent.kt
+++ server/src/main/java/de/ingrid/igeserver/migrations/tasks/M085AddIndicesForParent.kt
@@ -23,7 +23,6 @@ import de.ingrid.igeserver.migrations.MigrationBase
 import de.ingrid.igeserver.persistence.postgresql.jpa.ClosableTransaction
 import jakarta.persistence.EntityManager
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 import org.springframework.transaction.PlatformTransactionManager
 
