847e00d4e17e8167499f058dd2534737e3e68343
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2026-05-12 16:07:50 +0200

    Add mock OAuth2 client config beans in `KeycloakMockConfig`

--- server/src/main/java/de/ingrid/igeserver/development/KeycloakMockConfig.kt
+++ server/src/main/java/de/ingrid/igeserver/development/KeycloakMockConfig.kt
@@ -24,6 +24,7 @@ import de.ingrid.igeserver.configuration.StaleAuthoritiesRegistry
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -32,6 +33,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter
@@ -41,6 +43,42 @@ import org.springframework.web.filter.OncePerRequestFilter
 @Profile("dev")
 @Configuration
 internal class KeycloakMockConfig(val developmentAuthenticationFilter: DevelopmentAuthenticationFilter) : KeycloakConfig() {
+
+    @Bean
+    fun clientRegistrationRepository(): ClientRegistrationRepository = object : ClientRegistrationRepository, Iterable<org.springframework.security.oauth2.client.registration.ClientRegistration> {
+        override fun findByRegistrationId(registrationId: String?): org.springframework.security.oauth2.client.registration.ClientRegistration? = null
+        override fun iterator(): Iterator<org.springframework.security.oauth2.client.registration.ClientRegistration> = emptyList<org.springframework.security.oauth2.client.registration.ClientRegistration>().iterator()
+    }
+
+    @Bean
+    fun oauth2AuthorizedClientRepository(): OAuth2AuthorizedClientRepository = object : OAuth2AuthorizedClientRepository {
+        override fun <T : org.springframework.security.oauth2.client.OAuth2AuthorizedClient?> loadAuthorizedClient(registrationId: String?, authentication: org.springframework.security.core.Authentication?, request: HttpServletRequest?): T? = null
+        override fun saveAuthorizedClient(authorizedClient: org.springframework.security.oauth2.client.OAuth2AuthorizedClient?, authentication: org.springframework.security.core.Authentication?, request: HttpServletRequest?, response: HttpServletResponse?) {}
+        override fun removeAuthorizedClient(registrationId: String?, authentication: org.springframework.security.core.Authentication?, request: HttpServletRequest?, response: HttpServletResponse?) {}
+    }
+
+    @Bean
+    fun oauth2ClientProperties(): OAuth2ClientProperties {
+        val properties = OAuth2ClientProperties()
+        val registration = OAuth2ClientProperties.Registration()
+        registration.clientId = "mock-client"
+        registration.clientSecret = "mock-secret"
+        properties.registration["keycloak"] = registration
+
+        val provider = OAuth2ClientProperties.Provider()
+        provider.authorizationUri = "http://localhost:8080/realms/mock/protocol/openid-connect/auth"
+        properties.provider["keycloak"] = provider
+
+        return properties
+    }
+
+    @Bean
+    override fun authorizedClientManager(
+        clientRegistrationRepository: ClientRegistrationRepository,
+        authorizedClientRepository: OAuth2AuthorizedClientRepository,
+    ): OAuth2AuthorizedClientManager = object : OAuth2AuthorizedClientManager {
+        override fun authorize(authorizeRequest: org.springframework.security.oauth2.client.OAuth2AuthorizeRequest): org.springframework.security.oauth2.client.OAuth2AuthorizedClient? = null
+    }
 
     /**
      * Secure appropriate endpoints
