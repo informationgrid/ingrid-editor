2240169d694556860cd9d7b263983f696577902e
Benjamin Röhrl <benjamin.roehrl@wemove.com>
2026-07-10 17:17:32 +0200

    Fix package declaration

--- server/src/test/kotlin/de/ingrid/igeserver/schema/ingrid_hmdk/InGridPublicationHmdkSchema.kt
+++ server/src/test/kotlin/de/ingrid/igeserver/schema/ingrid_hmdk/InGridPublicationHmdkSchema.kt
@@ -17,7 +17,7 @@
  * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
-package de.ingrid.igeserver.schema.ingrid
+package de.ingrid.igeserver.schema.ingrid_hmdk
 
 import de.ingrid.igeserver.schema.SchemaUtils
 import io.kotest.core.spec.style.AnnotationSpec
