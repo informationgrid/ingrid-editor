756969c55d06cb26c59c01feab2be60d660653e1
André Wallat <andre.wallat@wemove.com>
2026-07-14 11:58:30 +0200

    Add validation for urlDataType (#8648)

--- frontend/src/profiles/ingrid/doctypes/ingrid-shared.ts
+++ frontend/src/profiles/ingrid/doctypes/ingrid-shared.ts
@@ -1701,6 +1701,7 @@ export abstract class IngridShared extends BaseDoctype {
             },
           }),
           requiredFieldsInItems: {
+            // required: type and title, when url then no uuidRef, when uuidRef then no url, when Datendownload then urlDataType
             expression: (ctrl: FormControl) =>
               !ctrl.value ||
               ctrl.value.length === 0 ||
@@ -1709,7 +1710,9 @@ export abstract class IngridShared extends BaseDoctype {
                   row.type &&
                   row.title?.length > 0 &&
                   ((row.url?.length > 0 && row.uuidRef == null) ||
-                    (row.url == null && row.uuidRef?.length > 0)),
+                    (row.url == null && row.uuidRef?.length > 0)) &&
+                  (row.type?.key !== "9990" ||
+                    row.urlDataType?.key?.length > 0),
               ),
             message:
               "Es müssen alle Pflichtfelder in den Verweisen ausgefüllt sein",
