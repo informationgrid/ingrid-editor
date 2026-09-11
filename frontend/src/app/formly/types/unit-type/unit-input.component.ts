53c420c2ef2b5f93e2d080462b8e4a614befd6a4
André Wallat <andre.wallat@wemove.com>
2026-09-11 12:01:03 +0200

    Fix unit-input component
    The change from #9465 introduced a new bug.

--- frontend/src/app/formly/types/unit-type/unit-input.component.ts
+++ frontend/src/app/formly/types/unit-type/unit-input.component.ts
@@ -93,11 +93,12 @@ export class UnitInputComponent
         });
 
       this.field.fieldGroup[0].formControl.valueChanges
-        .pipe(takeUntilDestroyed(this.destroyRef))
+        .pipe(takeUntilDestroyed(this.destroyRef), debounceTime(0))
         .subscribe((value: string) => {
-          this.field.fieldGroup[1].formControl.setValue(
-            value == null ? undefined : unitValue,
-          );
+          const unitControl = this.field.fieldGroup[1].formControl;
+          if (unitControl.value === undefined) {
+            unitControl.setValue(value == null ? undefined : unitValue);
+          }
         });
     });
   }
